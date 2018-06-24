f69a424450561f2958b551badfe7c70f6bdd169c
GNHua <gonghuabupt@gmail.com>
2018-06-23 22:13:24 -0600

    add logging

--- printer_server/autoapp.py
+++ printer_server/autoapp.py
@@ -1,10 +1,25 @@
 # -*- coding: utf-8 -*-
 """Create an application instance."""
+import os
+import logging
+import time
 from flask.helpers import get_debug_flag
 
 from printer_server.app import create_app
 from printer_server.settings import DevConfig, ProdConfig
 
 CONFIG = DevConfig if get_debug_flag() else ProdConfig
+
+logger = logging.getLogger('werkzeug')
+logger_filename = os.path.join()
+    CONFIG.PROJECT_ROOT, 
+    'logs', 
+    '{}.log'.format(time.strftime('%Y%m%d'))
+)
+format = '%(asctime)s - %(levelname)s - %(message)s'
+logging.basicConfig(filename=logger_filename,
+                    filemode='a',
+                    format=format,
+                    level=logging.INFO)
 
 app = create_app(CONFIG)
