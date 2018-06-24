e29056646b5e4a466c3313ca2d932728577e54f6
GNHua <gonghuabupt@gmail.com>
2018-06-23 23:48:44 -0600

    modify the bash script to locatee itself

--- printer_server/rpi/start.sh
+++ printer_server/rpi/start.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-ROOT="/home/pi/Developer/Python/3D_printer_control"
-. $ROOT/env/bin/activate
-export FLASK_APP=$ROOT/printer_server/autoapp.py
+PROJECT_ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )"/../.. && pwd )"
+. $PROJECT_ROOT/env/bin/activate
+export FLASK_APP=$PROJECT_ROOT/printer_server/autoapp.py
 export FLASK_DEBUG=1
 flask run --host=0.0.0.0 --port=5000 &
