88b962bb5c22d4949533becc0662caac7b38b05c
GNHua <gonghuabupt@gmail.com>
2018-06-24 23:48:13 -0600

    test on rpi

--- printer_server/rpi/start.sh
+++ printer_server/rpi/start.sh
@@ -3,5 +3,4 @@
 PROJECT_ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )"/../.. && pwd )"
 . $PROJECT_ROOT/env/bin/activate
 export FLASK_APP=$PROJECT_ROOT/printer_server/autoapp.py
-export FLASK_DEBUG=1
-flask run --host=0.0.0.0 --port=5000 &
+python $FLASK_APP > /dev/null &
