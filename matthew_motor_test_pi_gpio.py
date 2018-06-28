657b423a44ff547c5f9f8e7d63ec98bb5e880c6f
Matthew Viglione <matthewviglione@gmail.com>
2018-06-28 14:03:25 -0600

    Add limit switch

--- matthew_motor_test_pi_gpio.py
+++ matthew_motor_test_pi_gpio.py
@@ -21,6 +21,7 @@ import time
 # pin declarations 
 PULSE_PIN = 23      # green  (gnd)  yellow (+)
 DIRECTION_PIN = 24  # purple (gnd)  blue   (+)
+LIMIT_PIN = 18      # limit switch, goes high when pressed 
 
 # important delays 
 DIRECTION_CHANGE_DELAY = .001    # 100us, must be at least 5us (direction setup time)
@@ -64,21 +65,29 @@ try:
     # set pin directions 
     pi.set_mode(PULSE_PIN, pigpio.OUTPUT)
     pi.set_mode(DIRECTION_PIN, pigpio.OUTPUT)
+    pi.set_mode(LIMIT_PIN, pigpio.INPUT)
 
     # pi.set_pull_up_down(PULSE_PIN, pigpio.PUD_DOWN)
     # pi.set_pull_up_down(DIRECTION_PIN, pigpio.PUD_DOWN)
 
     while True: 
 
-        input_direction = input("Direction? 1-up, 0-down ")
-        input_steps     = input("How many steps? ")
-        input_delay     = input("Delay? (in us, >= 2.5) ")
+        # input_direction = input("Direction? 1-up, 0-down ")
+        # input_steps     = input("How many steps? ")
+        # input_delay     = input("Delay? (in us, >= 2.5) ")
         
-        input_direction = int(input_direction)    
-        input_steps = int(input_steps) 
-        input_delay = int(input_delay) 
+        # input_direction = int(input_direction)    
+        # input_steps = int(input_steps) 
+        # input_delay = int(input_delay) 
 
-        z_move(input_direction, input_steps, input_delay)
+        # z_move(input_direction, input_steps, input_delay)
+
+        curr_direction = 1; 
+
+        if pi.read(LIMIT_PIN):
+            z_move(curr_direction, 32000, 10)
+            curr_direction = not curr_direction 
+
 
 
 
