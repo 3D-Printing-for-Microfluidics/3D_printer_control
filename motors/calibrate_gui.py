dc7f64e1fc829ebaf070b68e20e10aa8669c078f
matthew-viglione <matthewviglione@gmail.com>
2018-07-20 19:24:01 -0600

    Add test sequence to GUI

--- motors/calibrate_gui.py
+++ motors/calibrate_gui.py
@@ -3,7 +3,7 @@ from functools import partial
 import RPi.GPIO as GPIO
 import time
 
-class CalibrateControl:
+class CalibrationControl:
 
     def __init__(self, master):
 
@@ -17,57 +17,64 @@ class CalibrateControl:
         self.labels_dynamic_text = []
 
         # Button parameters 
-        self.motors = ["Tip","Tilt","Distance"]
-        self.button_text = ["-1000","-100","-10","-1","+1","+10","+100", "+1000"]
+        self.motors = ["Tip","Tilt","Distance"] # if you add a motor here, make sure to add it's pins below 
+        self.button_text = ["-100","-10","-1","+1","+10","+100"]
 
         # Motor parameters 
         self.spr = 4096     # From spec sheet we know that the motors have 4096 steps per revolution
         self.dpr = 381      # We also know distance per revolution to be 381 um
-        self.delay = .004   # Min delay for 28BYJ-48 is 2 ms, we will set it at 4ms
+        self.delay = .003   # Min delay for 28BYJ-48 is 2 ms, we will set it at 4ms
 
         # GPIO pins used for each motor 
-        self.pin_names = ['A1','A2','B1','B2']
+        self.pin_names = ['IN1','IN2','IN3','IN4']
         self.pins = {
-            self.motors[0]:{              # tip 
-                self.pin_names[0]:1,    #  A1 
-                self.pin_names[1]:7,    #  A2 
-                self.pin_names[2]:8,    #  B1
-                self.pin_names[3]:25    #  B2 
+            self.motors[0]:{            # tip 
+                self.pin_names[0]:23,    
+                self.pin_names[1]:24,   
+                self.pin_names[2]:22,   
+                self.pin_names[3]:10    
             },
-            self.motors[1]:{              # tilt 
-                self.pin_names[0]:2,    #  A1 
-                self.pin_names[1]:3,    #  A2 
-                self.pin_names[2]:4,    #  B1
-                self.pin_names[3]:5     #  B2 
+            self.motors[1]:{            # tilt 
+                self.pin_names[0]:15,   
+                self.pin_names[1]:18,    
+                self.pin_names[2]:17,  
+                self.pin_names[3]:27    
             },
-            self.motors[2]:{              # distance 
-                self.pin_names[0]:6,    #  A1 
-                self.pin_names[1]:9,    #  A2 
-                self.pin_names[2]:10,   #  B1
-                self.pin_names[3]:11    #  B2 
+            self.motors[2]:{            # distance 
+                self.pin_names[0]:4,    
+                self.pin_names[1]:3,     
+                self.pin_names[2]:2,    
+                self.pin_names[3]:14    
             }
         }
 
         # Create and add buttons and labels to GUI
-        for m in range(len(self.motors)):
+        Label(master, text="Motor").grid(row=0, column=0)
+        Label(master, text="Location").grid(row=0, column=1)
+        Label(master, text="Move").grid(row=0, column=2, columnspan=len(self.button_text))
+        
+        for m in range(len(self.motors)):   # for each motor 
             # add m spot in the measurement array to hold the current value
             self.measurements.append(0)
 
             # add first column labels
             self.labels.append(Label(master, text=self.motors[m]))
-            self.labels[m].grid(row=m, column=0, sticky=W)
+            self.labels[m].grid(row=m+1, column=0, sticky=E)
 
             # add second column dynamic labels
             self.labels_dynamic_text.append(IntVar())
             self.labels_dynamic_text[m].set(self.measurements[m])
             self.labels_dynamic.append(Label(master, textvariable=self.labels_dynamic_text[m]))
-            self.labels_dynamic[m].grid(row=m, column=1)
+            self.labels_dynamic[m].grid(row=m+1, column=1)
 
             # add buttons
             for i in range(len(self.button_text)):
                 button_press_func = partial(self.update_labels, m, i)
                 self.buttons.append(Button(master, text=self.button_text[i], command=button_press_func))
-                self.buttons[len(self.button_text)*m+i].grid(column=i+2, row=m)
+                self.buttons[len(self.button_text)*m+i].grid(column=i+2, row=m+1)
+
+        test_button = Button(master, text="Run test sequence", command=self.test_sequence)
+        test_button.grid(column=len(self.button_text)-1, row=len(self.motors)+1, columnspan=3)
 
         # Setup GPIO
         GPIO.setmode(GPIO.BCM)   
@@ -105,17 +112,48 @@ class CalibrateControl:
         # This turns off current to the coils so the motor does not get hot
         self.setStep(motor, [0, 0, 0, 0]) 
 
+    def __del__(self):
+        try: 
+            GPIO.cleanup()  # always close GPIO pins when done 
+        except:
+            pass
+
     # Move motors and update dynamic labels. Called on button clicks   
     def update_labels(self, motor, value):
         valueInt = int(float(self.button_text[value]))                  # convert button text to int 
         self.measurements[motor] += valueInt                            # add/subtract 
-        print("move(",self.delay, self.pin_names[motor], valueInt,")")
         self.move(self.delay, self.motors[motor], valueInt)             # move motor 
         self.labels_dynamic_text[motor].set(self.measurements[motor])   # update the associated dynamic label 
 
-root = Tk()
-my_gui = CalibrateControl(root)
-root.mainloop()
+    # Iteratively toggle each input to each motor. 
+    # You should be able to watch the LEDs flash in order across all the motors   
+    def test_sequence(self):
+        delay = .1
+        for m in self.motors:
+            self.setStep(m, [1,0,0,0])
+            time.sleep(delay)
+            self.setStep(m, [0,1,0,0])
+            time.sleep(delay)
+            self.setStep(m, [0,0,1,0])
+            time.sleep(delay)
+            self.setStep(m, [0,0,0,1])
+            time.sleep(delay)
+            self.setStep(m, [0,0,1,0])
+            time.sleep(delay)
+            self.setStep(m, [0,1,0,0])
+            time.sleep(delay)
+            self.setStep(m, [1,0,0,0])
+            time.sleep(delay)
+            self.setStep(m, [0,0,0,0])
+            time.sleep(delay)
+    
+if __name__ == '__main__':
+    try:
+        root = Tk()
+        controller = CalibrationControl(root)
+        root.mainloop()
+    except:
+        GPIO.cleanup()
 
 # # This code can optionally be used to create an entry box
 # vcmd = master.register(self.validate) # we have to wrap the command
