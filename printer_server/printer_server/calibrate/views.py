8f54a42ec29fd29c1594ff2a5e9dc74cdeed253b
matthew-viglione <matthewviglione@gmail.com>
2018-08-07 12:26:19 -0600

    Add more tests for calibration sockets

--- printer_server/printer_server/calibrate/views.py
+++ printer_server/printer_server/calibrate/views.py
@@ -19,134 +19,140 @@ from printer_server.extensions import socketio
 blueprint = Blueprint('calibrate', __name__, url_prefix='/', static_folder='../static')
 
 # Decorator to handle navigation to calibration page 
-@blueprint.route('/calibrate')
+@blueprint.route('/calibrate1')
 def index():
     return render_template('calibrate.html')
 
 
+# @socketio.on('test1', namespace='/calibrate')
+# def test1():
+#     print("page ready")
+#     time.sleep(1)
+#     print("sending response")
+#     socketio.emit('matthew', namespace='/calibrate')
+
 @socketio.on('connect', namespace='/calibrate')
 def connect():
-    print("page ready")
-    time.sleep(3)
-    # socketio.emit('matthew', namespace='/calibrate')
-    print("sending response")
-    socketio.emit('matthew', namespace='/calibrate')
+    print("on connect")
+    time.sleep(1)
+    print("emit test1")
+    socketio.emit('test1', namespace='/calibrate')
 
 
-@socketio.on('initialize', namespace='/calibrate')
-def initialize(message):
-    if printer3d.state is 'uninitialized':
-        printingThreads.initialize()
+# @socketio.on('initialize', namespace='/calibrate')
+# def initialize(message):
+#     if printer3d.state is 'uninitialized':
+#         printingThreads.initialize()
 
 
-@socketio.on('planarization step 1', namespace='/calibrate')
-def planarizationStep1(message):
-    printer_states = ['initialized', 'planarized', 'completed', 'stopped']
-    if printer3d.state in printer_states:
-        printingThreads.planarizationStep1()
+# @socketio.on('planarization step 1', namespace='/calibrate')
+# def planarizationStep1(message):
+#     printer_states = ['initialized', 'planarized', 'completed', 'stopped']
+#     if printer3d.state in printer_states:
+#         printingThreads.planarizationStep1()
 
 
-@socketio.on('start', namespace='/calibrate')
-def start(message):
-    if printer3d.state is 'planarized':
-        jobId = message['job']
+# @socketio.on('start', namespace='/calibrate')
+# def start(message):
+#     if printer3d.state is 'planarized':
+#         jobId = message['job']
         
-        # if jobId:
-        #     # Prepares and archive all the files and information needed for the print job
-        #     job = PrintJob.query.get(jobId)
-        #     if not job:
-        #         return
+#         # if jobId:
+#         #     # Prepares and archive all the files and information needed for the print job
+#         #     job = PrintJob.query.get(jobId)
+#         #     if not job:
+#         #         return
             
-        #     # Removes the `current_job` folder to get a fresh start
-        #     try:
-        #         shutil.rmtree(os.path.join(Config.UPLOAD_FOLDER, 'current_job'))
-        #     except FileNotFoundError:
-        #         pass
+#         #     # Removes the `current_job` folder to get a fresh start
+#         #     try:
+#         #         shutil.rmtree(os.path.join(Config.UPLOAD_FOLDER, 'current_job'))
+#         #     except FileNotFoundError:
+#         #         pass
                 
-        #     _zipFile = os.path.join(Config.UPLOAD_FOLDER, 'queue', job.zip_filename)
-        #     with ZipFile(_zipFile, 'r') as f:
-        #         f.extractall(path=os.path.join(Config.UPLOAD_FOLDER, 'current_job'))
-        #         # Removes hidden files from Mac
-        #         try:
-        #             shutil.rmtree(os.path.join(Config.UPLOAD_FOLDER, 'current_job', '__MACOSX'))
-        #         except FileNotFoundError:
-        #             pass
+#         #     _zipFile = os.path.join(Config.UPLOAD_FOLDER, 'queue', job.zip_filename)
+#         #     with ZipFile(_zipFile, 'r') as f:
+#         #         f.extractall(path=os.path.join(Config.UPLOAD_FOLDER, 'current_job'))
+#         #         # Removes hidden files from Mac
+#         #         try:
+#         #             shutil.rmtree(os.path.join(Config.UPLOAD_FOLDER, 'current_job', '__MACOSX'))
+#         #         except FileNotFoundError:
+#         #             pass
                     
-        #     # Moves the zip file in `queue` folder to `print_history` folder
-        #     os.rename(_zipFile, os.path.join(Config.UPLOAD_FOLDER, 'print_history', job.zip_filename))
+#         #     # Moves the zip file in `queue` folder to `print_history` folder
+#         #     os.rename(_zipFile, os.path.join(Config.UPLOAD_FOLDER, 'print_history', job.zip_filename))
             
-        #     # # Saves a print record in the database
-        #     # printRecord = PrintRecord(original_filename=job.original_filename,
-        #     #                           upload_time = job.upload_time,
-        #     #                           upload_ip = job.upload_ip,
-        #     #                           start_ip=request.remote_addr)
-        #     # printRecord.save(commit=False)
+#         #     # # Saves a print record in the database
+#         #     # printRecord = PrintRecord(original_filename=job.original_filename,
+#         #     #                           upload_time = job.upload_time,
+#         #     #                           upload_ip = job.upload_ip,
+#         #     #                           start_ip=request.remote_addr)
+#         #     # printRecord.save(commit=False)
             
-        #     # Sends a `job selected` message to clients via sockets
-        #     message = {
-        #         'job': jobId,
-        #         'time': datetime.now().strftime('%Y-%m-%d %H:%M:%S'),
-        #         'text': 'Print Job ({}) selected'.format(job.original_filename)
-        #     }
-        #     # Once the job is selected and started, it will be deleted for queue.
-        #     # Therefore, we can use the `job deleted` event here, but with a 
-        #     # different message. 
-        #     socketio.emit('job deleted', message, 
-        #         namespace='/calibrate', broadcast=True)
-        #     job.delete()
+#         #     # Sends a `job selected` message to clients via sockets
+#         #     message = {
+#         #         'job': jobId,
+#         #         'time': datetime.now().strftime('%Y-%m-%d %H:%M:%S'),
+#         #         'text': 'Print Job ({}) selected'.format(job.original_filename)
+#         #     }
+#         #     # Once the job is selected and started, it will be deleted for queue.
+#         #     # Therefore, we can use the `job deleted` event here, but with a 
+#         #     # different message. 
+#         #     socketio.emit('job deleted', message, 
+#         #         namespace='/calibrate', broadcast=True)
+#         #     job.delete()
             
-        #     printSettingsFile = glob.glob(os.path.join(Config.UPLOAD_FOLDER, 
-        #         'current_job', '**/print_settings.json'), recursive=True)[0]
-        #     printingThreads.printSettings = PrintSettings.fromFile(printSettingsFile)
-        #     printingThreads.jsonDir = os.path.dirname(printSettingsFile)
-        #     printingThreads.start()
+#         #     printSettingsFile = glob.glob(os.path.join(Config.UPLOAD_FOLDER, 
+#         #         'current_job', '**/print_settings.json'), recursive=True)[0]
+#         #     printingThreads.printSettings = PrintSettings.fromFile(printSettingsFile)
+#         #     printingThreads.jsonDir = os.path.dirname(printSettingsFile)
+#         #     printingThreads.start()
 
 
-@socketio.on('pause', namespace='/calibrate')
-def pause(message):
-    if printer3d.state is 'printing':
-        printingThreads.pause()
+# @socketio.on('pause', namespace='/calibrate')
+# def pause(message):
+#     if printer3d.state is 'printing':
+#         printingThreads.pause()
 
 
-@socketio.on('resume', namespace='/calibrate')
-def resume(message):
-    if printer3d.state is 'paused':
-        printingThreads.resume()
+# @socketio.on('resume', namespace='/calibrate')
+# def resume(message):
+#     if printer3d.state is 'paused':
+#         printingThreads.resume()
 
 
-@socketio.on('stop', namespace='/calibrate')
-def stop(message):
-    if printer3d.state is 'printing' or printer3d.state is 'paused':
-        printingThreads.stop()
+# @socketio.on('stop', namespace='/calibrate')
+# def stop(message):
+#     if printer3d.state is 'printing' or printer3d.state is 'paused':
+#         printingThreads.stop()
 
 
-@socketio.on('shutdown', namespace='/calibrate')
-def shutdown(message):
-    if printer3d.state not in ['busy', 'printing']:
-        message = {
-            'time': datetime.now().strftime('%Y-%m-%d %H:%M:%S'),
-            'text': 'Shutting down'
-        }
-        socketio.emit('shutting down', message,
-            namespace='/calibrate', broadcast=True)
+# @socketio.on('shutdown', namespace='/calibrate')
+# def shutdown(message):
+#     if printer3d.state not in ['busy', 'printing']:
+#         message = {
+#             'time': datetime.now().strftime('%Y-%m-%d %H:%M:%S'),
+#             'text': 'Shutting down'
+#         }
+#         socketio.emit('shutting down', message,
+#             namespace='/calibrate', broadcast=True)
 
-        func = request.environ.get('werkzeug.server.shutdown')
-        if func is None:
-            raise RuntimeError('Not running with the Werkzeug Server')
+#         func = request.environ.get('werkzeug.server.shutdown')
+#         if func is None:
+#             raise RuntimeError('Not running with the Werkzeug Server')
             
-        printer3d.solus.__del__()
-        printer3d.projector.__del__()
+#         printer3d.solus.__del__()
+#         printer3d.projector.__del__()
         
-        socketio.emit('shutdown completed', dict(),
-            namespace='/calibrate', broadcast=True)
-        time.sleep(1)
-        func()
+#         socketio.emit('shutdown completed', dict(),
+#             namespace='/calibrate', broadcast=True)
+#         time.sleep(1)
+#         func()
 
-    else:
-        message = {
-            'time': datetime.now().strftime('%Y-%m-%d %H:%M:%S'),
-            'text': "Try to shutdown 3D printer when it's busy"
-        }
-        socketio.emit('shutdown failed', message,
-            namespace='/calibrate', broadcast=True)
+#     else:
+#         message = {
+#             'time': datetime.now().strftime('%Y-%m-%d %H:%M:%S'),
+#             'text': "Try to shutdown 3D printer when it's busy"
+#         }
+#         socketio.emit('shutdown failed', message,
+#             namespace='/calibrate', broadcast=True)
 
--- printer_server/printer_server/static/js/calibrate.js
+++ printer_server/printer_server/static/js/calibrate.js
@@ -17,21 +17,21 @@ var enable_solus_buttons = function(){
 
 
 
-var update_print_message = function(message) {
-    if(!$.isEmptyObject(message)) {
-        var new_text = `
-        <div class="row">
-            <div class="col-4">
-            ${message.time}
-            </div>
-            <div class="col-8">
-            ${message.text}
-            </div>
-        </div>
-        `;
-        $("#print-message").append(new_text);
-    }
-}
+// var update_print_message = function(message) {
+//     if(!$.isEmptyObject(message)) {
+//         var new_text = `
+//         <div class="row">
+//             <div class="col-4">
+//             ${message.time}
+//             </div>
+//             <div class="col-8">
+//             ${message.text}
+//             </div>
+//         </div>
+//         `;
+//         $("#print-message").append(new_text);
+//     }
+// }
 
 $(document).ready(function(){
     
@@ -49,58 +49,67 @@ $(document).ready(function(){
     
     // Set up socket 
     var socket = io.connect("http://" + document.domain + ":" + location.port + "/calibrate");
+    console.log("http://" + document.domain + ":" + location.port + "/calibrate")
     
     
     disable_motor_control_buttons();
+
+    console.log("emit connect");
     socket.emit("connect");
+
+
+
+    // console.log("emit test1")
+    // socket.emit("test1");
     
-    socket.on("matthew", function() {
+    socket.on("test1", function() {
+        console.log("on test1");
         enable_motor_control_buttons();
     });
 
-    socket.on("busy", function(message) {
-        $("#printer-state").text("3D Printer is Busy");
-        // show_btn();
-        start_job_id = "";
-        $(".clickable-row").removeClass("table-success");
-        update_print_message(message);
-    });
+    // socket.on("busy", function(message) {
+    //     $("#printer-state").text("3D Printer is Busy");
+    //     // show_btn();
+    //     start_job_id = "";
+    //     $(".clickable-row").removeClass("table-success");
+    //     update_print_message(message);
+    // });
     
-    socket.on("uninitialized", function(message) {
-        $("#printer-state").text("Uninitialized");
-        // show_btn("#init-btn, #shutdown-btn");
-    });
+    // socket.on("uninitialized", function(message) {
+    //     $("#printer-state").text("Uninitialized");
+    //     // show_btn("#init-btn, #shutdown-btn");
+    // });
     
 
-    socket.on("shutdown completed", function(message) {
-        $("html").text("3D printer has been shutdown");
-    });
+    // socket.on("shutdown completed", function(message) {
+    //     $("html").text("3D printer has been shutdown");
+    // });
 
-    socket.on("shutdown failed", function(message) {
-        // TODO: add a warning window
-        update_print_message(message);
-    });
+    // socket.on("shutdown failed", function(message) {
+    //     // TODO: add a warning window
+    //     update_print_message(message);
+    // });
 
 
     
-    $("#print-alert-confirm").click(function() {
-        var operation = $("#print-alert-title").text();
-        var msg;
+    // $("#print-alert-confirm").click(function() {
+    //     var operation = $("#print-alert-title").text();
+    //     var msg;
         
-        if (operation === "Start") {
-            msg = {job: start_job_id};
-        } else if (operation === "Delete Job") {
-            msg = {job: delete_job_id};
-        } else {
-            msg = {};
-        }
-        socket.emit(operation.toLowerCase(), msg);
-        $("#print-alert-title").text("");
-        $("#print-alert-body").text("");
-    });
+    //     if (operation === "Start") {
+    //         msg = {job: start_job_id};
+    //     } else if (operation === "Delete Job") {
+    //         msg = {job: delete_job_id};
+    //     } else {
+    //         msg = {};
+    //     }
+    //     socket.emit(operation.toLowerCase(), msg);
+    //     $("#print-alert-title").text("");
+    //     $("#print-alert-body").text("");
+    // });
 
-    $("#shutdown-btn").click(function() {
-        $("#print-alert-title").text("Shutdown");
-        $("#print-alert-body").text("Make sure 3D printer is not operating, and that build platform is at home position.");
-    });
+    // $("#shutdown-btn").click(function() {
+    //     $("#print-alert-title").text("Shutdown");
+    //     $("#print-alert-body").text("Make sure 3D printer is not operating, and that build platform is at home position.");
+    // });
 });
