d3db04ce1faa12cccf69b7aab5870e3fa343c08a
matthew-viglione <matthewviglione@gmail.com>
2018-08-09 17:49:59 -0600

    Add image uploading capabiility to light engine control

--- printer_server/printer_server/calibrate/views.py
+++ printer_server/printer_server/calibrate/views.py
@@ -1,7 +1,10 @@
 # -*- coding: utf-8 -*-
 """Control view."""
-from flask import Blueprint, render_template
+import os
+from flask import Blueprint, request, render_template
+from datetime import datetime
 
+from printer_server.settings import CalibrationConfig
 from printer_server.hardware import printer3d
 from printer_server.threads import calibrationThreads
 from printer_server.extensions import socketio
@@ -33,3 +36,11 @@ def solus_go_to_bottom():
 @socketio.on('calibration_motor', namespace='/calibrate')
 def calibration_motor_move(message):
     calibrationThreads.calibration_motor_move(message["axis"],message["steps"])
+
+@blueprint.route('handle-calibration-upload', methods=['POST'])
+def handleUpload():
+    file = request.files.getlist('file')[0]
+    newFilename = os.path.join(CalibrationConfig.UPLOAD_FOLDER, 'calibration_images','temp.png')
+    file.save(newFilename)
+    socketio.emit('calibration_image_uploaded', namespace='/calibrate', broadcast=True)
+    return ''   # this line is requred as a response must be returned 
--- printer_server/printer_server/static/js/calibrate.js
+++ printer_server/printer_server/static/js/calibrate.js
@@ -1,3 +1,10 @@
+var disable_all_buttons = function(){
+    $('button').prop('disabled', true);
+}
+
+var enable_all_buttons = function(){
+    $('button').prop('disabled', false);
+}
 
 var disable_calibration_motor_buttons = function(){
     $('.motor-controls button').prop('disabled', true);
@@ -15,69 +22,108 @@ var enable_solus_buttons = function(){
     $('.solus button').prop('disabled', false);
 }
 
-var disable_all_buttons = function(){
-    $('button').prop('disabled', true);
+var enable_upload_buttons = function(){
+    $('.light-engine button').prop('disabled', false);
 }
 
-var enable_all_buttons = function(){
-    $('button').prop('disabled', false);
+var disable_upload_buttons = function(){
+    $('.light-engine button').prop('disabled', true);
 }
 
 $(document).ready(function(){
     
-    var slider = document.getElementById("myRange");
-    var output = document.getElementById("myValue");
-    output.innerHTML = slider.value; // Display the default slider value
-
-    // Update the current slider value (each time you drag the slider handle)
-    slider.oninput = function() {
-        output.innerHTML = this.value;
-    }
-        
-    // Set up socket and send message to initialize hardware 
+    // Set up socket, disable all controls, and send message to initialize hardware 
     var socket = io.connect("http://" + document.domain + ":" + location.port + "/calibrate");
-    
     disable_all_buttons();
     socket.emit("initialize");
 
+    // Handles to LedPower slider and label 
+    var LedPowerSliderElement = document.getElementById("LedPowerSlider");
+    var LedPowerLabelElement  = document.getElementById("LedPowerLabel");
+    
+    // Set initial LedPower slider label value 
+    LedPowerLabelElement.innerHTML = LedPowerSliderElement.value; // Display the default slider value
+
+    // Update the LedPowerLabelElement with the current slider value
+    LedPowerSliderElement.oninput = function() {
+        LedPowerLabelElement.innerHTML = this.value;
+    }
+
+    // Once hardware is initialized, enable controls 
     socket.on("initialized", function(){
         enable_all_buttons();
     });
-
-    var tipLabel  = document.getElementById("tip-state");
-    var tiltLabel = document.getElementById("tilt-state");
-    var distLabel = document.getElementById("dist-state");
-
-    $(".mtr-cntrl-btn").click(function() {
-        var steps = $(this).text(); 
-        var axis = $(this).parent().attr('aria-label')
-        if (axis == "Tip") {
-            tipLabel.innerHTML = Number(tipLabel.innerHTML) + Number(steps); 
-        } else if (axis == "Tilt") {
-            tiltLabel.innerHTML = Number(tiltLabel.innerHTML) + Number(steps); 
-        } else if (axis == "Distance") {
-            distLabel.innerHTML = Number(distLabel.innerHTML) + Number(steps); 
-        }
-        disable_calibration_motor_buttons();
-        socket.emit("calibration_motor", {"axis": axis, "steps": steps});
+    
+    // Enable calibration motor buttons when current motion is complete 
+    socket.on("calibration_motor_done", function() {
+        enable_calibration_motor_buttons();
+    });
+    
+    // Enable Solus control buttons when current Solus motion is complete 
+    socket.on("solus_done", function() {
+        enable_solus_buttons();
     });
 
+    // Once once upload is complete, re-enable upload controls  
+    socket.on("calibration_image_uploaded", function(){
+        enable_upload_buttons();
+    });
+    
+    // Upload button click function 
+    $("#upload-btn").on("click", function(e) {
+        var selectedFile = document.getElementById('file-picker').files[0];
+        if (typeof selectedFile !== 'undefined') {  // if there is a file selected 
+            disable_upload_buttons();
+            uploadFile(selectedFile);
+        }
+        
+    });
+
+    // Solus control top button click function 
     $("#solus-top-btn").click(function() {
         disable_solus_buttons();
         socket.emit("solus_go_to_top");
     });
 
+    // Solus control bottom button click function 
     $("#solus-bottom-btn").click(function() {
         disable_solus_buttons();
         socket.emit("solus_go_to_bottom");
     });
 
-    socket.on("solus_done", function() {
-        enable_solus_buttons();
-    });
+    // Handles to auto-updating position labels
+    var tipLabelElement  = document.getElementById("tip-state");
+    var tiltLabelElement = document.getElementById("tilt-state");
+    var distLabelElement = document.getElementById("dist-state");
 
-    socket.on("calibration_motor_done", function() {
-        enable_calibration_motor_buttons();
+    // Calibration motor control button click function  
+    $(".mtr-cntrl-btn").click(function() {
+        // Parse button content 
+        var steps = $(this).text(); 
+        var axis = $(this).parent().attr('aria-label')
+        if (axis == "Tip") {
+            tipLabelElement.innerHTML  = Number(tipLabelElement.innerHTML)  + Number(steps); 
+        } else if (axis == "Tilt") {
+            tiltLabelElement.innerHTML = Number(tiltLabelElement.innerHTML) + Number(steps); 
+        } else if (axis == "Distance") {
+            distLabelElement.innerHTML = Number(distLabelElement.innerHTML) + Number(steps); 
+        }
+        // Disable calibration motor buttons  
+        disable_calibration_motor_buttons();
+        // Emit control message with parsed values 
+        socket.emit("calibration_motor", {"axis": axis, "steps": steps});
     });
-    
 });
+
+function uploadFile(image) {
+    var fd = new FormData();    // Create form data 
+    fd.append("file", image);   // Attach the image file 
+    $.ajax({                    // Use ajax to compose and send the request  
+        url: "/handle-calibration-upload",
+        method: "POST",
+        contentType: false,
+        processData: false,
+        cache: false,
+        data: fd
+    });
+}
