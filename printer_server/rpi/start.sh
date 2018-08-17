f037b300c8fba772aa2651b4bc25b23bded43d59
matthew-viglione <matthewviglione@gmail.com>
2018-08-17 16:15:09 -0600

    Fix bug in display setup

--- printer_server/rpi/start.sh
+++ printer_server/rpi/start.sh
@@ -3,6 +3,9 @@
 PROJECT_ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )"/../.. && pwd )"
 . $PROJECT_ROOT/env/bin/activate
 export FLASK_APP=$PROJECT_ROOT/printer_server/autoapp.py
-date >> $PROJECT_ROOT/error.txt 
+export DISPLAY=:0.0
+date >> $PROJECT_ROOT/error_report.txt 
 python $FLASK_APP >> $PROJECT_ROOT/error.txt &
+# python -u $FLASK_APP > >(tee -a error_report.txt) 2> >(tee -a error_report.txt >&2) & 
+# python -u $FLASK_APP |& tee -a error_report.txt
 
