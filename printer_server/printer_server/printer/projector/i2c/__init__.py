c85c9eaa381efc96513fa7b222ae469aa4941466
matthew-viglione <19675897+matthew-viglione@users.noreply.github.com>
2019-10-09 20:19:07 -0600

    Remove old files

--- printer_server/printer_server/printer/projector/i2c/__init__.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-"""The i2c module"""
-
-import platform
-
-if platform.platform().startswith('Windows'):
-    from .windows.i2cdriver import LightEngineI2C
-elif platform.platform().startswith('Linux'):
-    from .rpi.i2cdriver import LightEngineI2C
