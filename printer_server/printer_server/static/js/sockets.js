03473611e979ee0aa29cabc7a49f59bc3f42839a
matthew-viglione <19675897+matthew-viglione@users.noreply.github.com>
2019-10-11 14:38:17 -0600

    Update web UI messages

--- printer_server/printer_server/static/js/sockets.js
+++ printer_server/printer_server/static/js/sockets.js
@@ -49,7 +49,7 @@ var update_print_message = function(message) {
 $(document).ready(function(){
     var socket = io.connect("http://" + document.domain + ":" + location.port + "/printing");
     socket.emit("connect");
-    
+
     socket.on("busy", function(message) {
         $("#printer-state").text("3D Printer is Busy");
         show_btn();
@@ -57,28 +57,28 @@ $(document).ready(function(){
         $(".clickable-row").removeClass("table-success");
         update_print_message(message);
     });
-    
+
     socket.on("uninitialized", function(message) {
         $("#printer-state").text("Uninitialized");
         show_btn("#init-btn, #shutdown-btn");
     });
-    
+
     socket.on("initialized", function(message) {
         $("#printer-state").text("Initialized");
         show_btn("#plana1-btn, #shutdown-btn, #admin-btn");
     });
-    
+
     socket.on("planarizing", function(message) {
         $("#printer-state").text("Planarizing");
         show_btn("#plana2-btn");
     });
-    
+
     socket.on("planarized", function(message) {
         $("#printer-state").text("Planarized");
         show_btn("#plana1-btn, #shutdown-btn");
         $("#start-btn").removeClass("d-none").addClass("btn-secondary");
     });
-    
+
     socket.on("printing", function(message) {
         $("#printer-state").text("Printing");
         show_btn("#pause-btn, #stop-btn");
@@ -88,25 +88,25 @@ $(document).ready(function(){
         $("#print-progress").removeClass("d-none");
         update_print_message(message);
     });
-    
+
     socket.on("print progress", function(message) {
         $("#print-progress-bar").css({"width": message.percent + "%"})
                                 .attr({"aria-valuenow": message.percent})
                                 .text(message.percent + "%");
         update_print_message(message);
     });
-    
+
     socket.on("paused", function(message) {
         $("#printer-state").text("Paused");
         show_btn("#resume-btn, #stop-btn");
     });
-    
+
     socket.on("stopped", function(message) {
         $("#printer-state").text("Stopped");
         show_btn("#plana1-btn, #shutdown-btn, #admin-btn");
         $("#print-progress").addClass("d-none");
     });
-    
+
     socket.on("completed", function(message) {
         $("#printer-state").text("Completed");
         show_btn("#plana1-btn, #shutdown-btn, #admin-btn");
@@ -129,11 +129,11 @@ $(document).ready(function(){
     });
 
 
-    
+
     $("#print-alert-confirm").click(function() {
         var operation = $("#print-alert-title").text();
         var msg;
-        
+
         if (operation === "Start") {
             msg = {job: start_job_id};
         } else if (operation === "Delete Job") {
@@ -153,44 +153,44 @@ $(document).ready(function(){
 
     $("#init-btn").click(function() {
         $("#print-alert-title").text("Initialize");
-        $("#print-alert-body").text("Is bluid platform taken off?");
+        $("#print-alert-body").text("WARNING: Is the bluid platform taken off? Initializing with the platform on can seriously damage the printer!");
     });
 
     $("#plana1-btn").click(function() {
         $("#print-alert-title").text("Planarization Step 1");
-        $("#print-alert-body").text("Is bluid platform mounted? (Make sure the build platform has been taken off for the previous print.)");
+        $("#print-alert-body").text("Is bluid platform mounted? (Make sure the previous print has been removed if applicable)");
     });
 
     $("#plana2-btn").click(function() {
         $("#print-alert-title").text("Planarization Step 2");
-        $("#print-alert-body").text("Make sure the build platform or silanized glass is flat on the film, and tighten the screws.");
+        $("#print-alert-body").text("Make sure the build platform or silanized glass is flat and tighten the screws.");
     });
 
     $("#start-btn").click(function() {
         $("#print-alert-title").text("Start");
-        $("#print-alert-body").text("Are you sure to start printing?");
+        $("#print-alert-body").text("Are you sure you want to start printing?");
     });
 
     $("#pause-btn").click(function() {
         $("#print-alert-title").text("Pause");
-        $("#print-alert-body").text("Are you sure to pause printing?");
+        $("#print-alert-body").text("Are you sure you want to pause printing?");
     });
 
     $("#resume-btn").click(function() {
         $("#print-alert-title").text("Resume");
-        $("#print-alert-body").text("Are you sure to resume printing?");
+        $("#print-alert-body").text("Are you sure you want to resume printing?");
     });
 
     $("#stop-btn").click(function() {
         $("#print-alert-title").text("Stop");
-        $("#print-alert-body").text("Are you sure to stop printing?");
+        $("#print-alert-body").text("Are you sure you want to stop printing?");
     });
 
     $("#shutdown-btn").click(function() {
         $("#print-alert-title").text("Shutdown");
-        $("#print-alert-body").text("Make sure 3D printer is not operating, and that build platform is at home position.");
+        $("#print-alert-body").text("Make sure 3D printer is not in operation.");
     });
-       
+
     // Database interaction
     socket.on("job uploaded", function(message) {
         var new_row = `
@@ -206,18 +206,18 @@ $(document).ready(function(){
         var new_msg = {time: message.uploadTime, text: "Print Job (" + message.name + ") Uploaded"};
         update_print_message(new_msg);
     });
-    
+
     $(document).on("click", ".delete-job", function() {
         delete_job_id = $(this).attr("id").replace("delete-job", "");
         $("#print-alert-title").text("Delete Job");
         $("#print-alert-body").text("Are you sure to delete this print job?");
     });
-    
+
     socket.on("job deleted", function(message) {
         $("#row-" + message.job).remove();
         update_print_message(message);
     });
-    
+
     socket.on("my error", function(message){
         var flash_msg = `
        <div class="alert alert-${message.category}">
