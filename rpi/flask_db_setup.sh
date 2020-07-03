acc19c05afe9713f3d01232b144f084f6f7acc74
matthew-viglione <19675897+matthew-viglione@users.noreply.github.com>
2020-07-02 18:28:08 -0600

    Fix execution path

--- rpi/flask_db_setup.sh
+++ rpi/flask_db_setup.sh
@@ -1,22 +1,25 @@
 #!/bin/sh
 
-# Set project root 
-PROJECT_ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )"/../.. && pwd )"
+# Set project root
+PROJECT_ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )"/.. && pwd )"
 
-# Activate virtual environment 
+# Activate virtual environment
 . $PROJECT_ROOT/env/bin/activate
 
-# Open printer server folder 
+# Open printer server folder
 cd $PROJECT_ROOT/printer_server
 
-# Remove old database if it exists 
+# Remove old database if it exists
 [ -e migrations ] && echo "Moving old migrations to migrations.bak" && mv migrations migrations.bak
 [ -e dev.db ] && echo "Moving old db to dev.db.bak" && mv dev.db dev.db.bak
 
-# # Set flask app pointer 
+# # Set flask app pointer
 export FLASK_APP=autoapp.py
 
-# # Create new database 
+# Create new database
 flask db init
 flask db migrate
 flask db upgrade
+
+# Go back to original folder
+cd $PROJECT_ROOT
