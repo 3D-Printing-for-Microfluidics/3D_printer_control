e64e13393e5e9914870e1eba46a9155eae26ea7c
matthew-viglione <19675897+matthew-viglione@users.noreply.github.com>
2020-07-03 15:16:40 -0600

    Consolidate main printing view

--- printer_server/app.py
+++ printer_server/app.py
@@ -6,7 +6,7 @@ from flask import Flask, render_template
 from flask.logging import default_handler
 from printer_server.extensions import db, migrate, socketio
 from printer_server import commands, models
-from printer_server.views import home, digital, manual
+from printer_server.views import home, manual
 from printer_server.settings import ProdConfig
 from printer_server.hardware import printer3d
 from printer_server.logging_handler import SQLAlchemyHandler
@@ -40,7 +40,6 @@ def register_extensions(app):
 def register_blueprints(app):
     """Register Flask blueprints."""
     app.register_blueprint(home.blueprint)
-    app.register_blueprint(digital.blueprint)
     app.register_blueprint(manual.blueprint)
 
 
--- printer_server/threads.py
+++
@@ -1,5 +0,0 @@
-# -*- coding: utf-8 -*-
-"""Thread module"""
-from printer_server.views.printing_threads import PrintingThreads
-
-printingThreads = PrintingThreads()
