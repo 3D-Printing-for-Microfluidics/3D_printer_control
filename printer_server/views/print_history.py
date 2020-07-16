35f8d5a658d2e2725e793153d9f9cd3f04f3a04c
matthew-viglione <19675897+matthew-viglione@users.noreply.github.com>
2020-07-16 13:05:39 -0600

    Add file download links

--- printer_server/views/print_history.py
+++ printer_server/views/print_history.py
@@ -1,23 +1,10 @@
 import os
-import time
-import glob
-import shutil
-import threading
-from pathlib import Path
-from zipfile import ZipFile
 from datetime import datetime
-from functools import wraps
-from flask import Blueprint, request, render_template, flash
+from flask import Blueprint, request, render_template, flash, send_file
 
 from printer_server.settings import Config
-from printer_server.hardware_configuration import hardware_driver_handles
+from printer_server.models import PrintRecord
 
-# from printer_server.print_settings import print_settings
-from printer_server.print_file_validator import validate_v02
-from printer_server.models import PrintQueue, PrintRecord
-from printer_server.extensions import db, socketio
-
-# Create bluprint
 blueprint = Blueprint(
     "print_history", __name__, url_prefix="/", static_folder="../static"
 )
@@ -49,23 +36,12 @@ def calcPageNum(currentPage, totalPage):
     return startPage, endPage
 
 
-# @blueprint.route("/print_history")
-# def index():
-#     allJobs = PrintQueue.query.all()
-#     return render_template("printing.html", allJobs=allJobs)
-
-
 @blueprint.route("/print_history")
 def printHistroy():
     page = request.args.get("page", 1, type=int)
 
     _PR = PrintRecord
     _q = _PR.query
-    print(_q)
-
-    # startDate = "2020-01-01"
-    # # endDate = datetime(2020, month=12, day=1)
-    # endDate = "2020-12-31"
 
     try:
         startDate = request.args.get("start", "")
@@ -74,7 +50,6 @@ def printHistroy():
             _startDate = datetime(*temp)
             _startDate = datetime(*temp)
             _q = _q.filter(_PR.start_time >= _startDate)
-        print(_q)
 
     except ValueError:
         flash("Incorrect start date", category="danger")
@@ -85,27 +60,12 @@ def printHistroy():
             temp = [int(i) for i in endDate.split("-")]
             _endDate = datetime(*temp)
             _q = _q.filter(_PR.start_time <= _endDate)
-            print(_q)
     except ValueError:
         flash("Incorrect end date", category="danger")
 
     recs = _q.order_by(_PR.id.desc()).paginate(page, 50)
     startPage, endPage = calcPageNum(page, recs.pages)
-    print("TOTAL", recs.total)
-    # print("TOTAL", recs.items)
-    for record in recs.items:
-        print(
-            record,
-            record.original_filename,
-            record.upload_time,
-            record.upload_time,
-            record.start_ip,
-            record.start_time,
-            record.end_time,
-            record.completed,
-        )
 
-    print(f"LOTS O STUFF\n{recs}\n{startPage}\n{endPage}\n{startDate}\n{endDate}\nDONE")
     return render_template(
         "print_history.html",
         recs=recs,
@@ -116,38 +76,13 @@ def printHistroy():
     )
 
 
-@blueprint.route("archive")
-def archive():
-    page = request.args.get("page", 1, type=int)
-
-    _PR = PrintRecord
-    _q = _PR.query
-
-    try:
-        startDate = request.args.get("start", "")
-        if startDate:
-            temp = [int(i) for i in startDate.split("-")]
-            _startDate = datetime(*temp)
-            _q = _q.filter(_PR.start_time >= _startDate)
-    except ValueError:
-        flash("Incorrect start date", category="danger")
-
-    try:
-        endDate = request.args.get("end", "")
-        if endDate:
-            temp = [int(i) for i in endDate.split("-")]
-            _endDate = datetime(*temp)
-            _q = _q.filter(_PR.start_time <= _endDate)
-    except ValueError:
-        flash("Incorrect end date", category="danger")
-
-    recs = _q.order_by(_PR.id.desc()).paginate(page, 50)
-    startPage, endPage = calcPageNum(page, recs.pages)
-    return render_template(
-        "archive.html",
-        recs=recs,
-        startPage=startPage,
-        endPage=endPage,
-        startDate=startDate,
-        endDate=endDate,
+@blueprint.route("/print_history/<path:job_id>", methods=["GET", "POST"])
+def download(job_id):
+    """Download the job specified by job_id."""
+    file_location = os.path.join(os.path.join(Config.UPLOAD_FOLDER, "print_history"))
+    job = PrintRecord.query.get_or_404(job_id)
+    return send_file(
+        os.path.join(file_location, job.zip_filename),
+        as_attachment=True,
+        attachment_filename=job.original_filename,
     )
