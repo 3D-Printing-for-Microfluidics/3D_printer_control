ca20c0a21680fc4868b5ae60433c300cd3565988
matthew-viglione <19675897+matthew-viglione@users.noreply.github.com>
2020-08-05 16:22:14 -0600

    Fix broken database link

--- printer_server/settings.py
+++ printer_server/settings.py
@@ -10,10 +10,11 @@ class Config(object):
     APP_DIR = os.path.abspath(os.path.dirname(__file__))  # This directory
     PROJECT_ROOT = os.path.abspath(os.path.join(APP_DIR, os.pardir))
     UPLOAD_FOLDER = os.path.abspath(os.path.join(PROJECT_ROOT, "upload"))
-    DB_NAME = "3d_printer_database.db"
-    DB_PATH = os.path.join(PROJECT_ROOT, DB_NAME)
     DEBUG_TB_ENABLED = False  # Disable Debug toolbar
     DEBUG_TB_INTERCEPT_REDIRECTS = False
+    DB_NAME = "3d_printer_database.db"
+    DB_PATH = os.path.join(PROJECT_ROOT, DB_NAME)
+    SQLALCHEMY_DATABASE_URI = "sqlite:///{0}".format(DB_PATH)
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 
@@ -40,6 +41,7 @@ class TestConfig(Config):
     TESTING = True
     DEBUG = True
     DB_PATH = os.path.join(Config.PROJECT_ROOT, "test.db")
+    SQLALCHEMY_DATABASE_URI = "sqlite:///{0}".format(DB_PATH)
     BCRYPT_LOG_ROUNDS = (
         4  # For faster tests; needs at least 4 to avoid "ValueError: Invalid rounds"
     )
