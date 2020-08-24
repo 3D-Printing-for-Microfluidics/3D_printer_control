a8a449e9e605ecd75536bb9ce4fab4bc60374248
Dallin Miner <keyofd.miner@gmail.com>
2020-08-24 14:38:36 -0600

    Started cleaning up code. Added logging.

--- printer_server/hardware_drivers/loadcell/loadcell.py
+++ printer_server/hardware_drivers/loadcell/loadcell.py
@@ -5,31 +5,26 @@ import serial.serialutil
 import threading
 import os
 
-def findUsbPort(hwid):
-    ports = list(serial.tools.list_ports.comports())
-    for p in ports:
-        if hwid.upper() in p.hwid:
-            print("Found '{}' at '{}'".format(p.hwid, p.device))
-            return p.device
-    return None             # not found
+import logging
 
-# class LoadCell():
 class LoadCellDeviceControl(serial.Serial):
-
-    #def __init__(self, hwid='16c0:0483', verbose=True):
-    def __init__(self, hwid='PID=16C0:0483 SER=6256240', verbose=True):
+    def __init__(self, hwid='PID=16C0:0483 SER=6256240', log_level=logging.DEBUG):
         super().__init__(baudrate=115200, timeout=1)
-        # Button parameters
-        self.verbose = verbose
+        self.log_level = log_level
         self.hwid = hwid
         self.port = None                # start with no port
         self.status = None              # status to be updated after every send
-
-
-        atexit.register(self.close)
+        
+    def findUsbPort(self, hwid):
+        ports = list(serial.tools.list_ports.comports())
+        for p in ports:
+            if hwid.upper() in p.hwid:
+                self.log.debug("Found '%s' at '%s'", p.hwid, p.device)
+                return p.device
+        return None             # not found
 
     def connect(self):
-        self.port = findUsbPort(self.hwid)
+        self.port = self.findUsbPort(self.hwid)
         if self.port is None:
             raise ValueError('Load cell not found')
         if self.is_open:
@@ -37,27 +32,33 @@ class LoadCellDeviceControl(serial.Serial):
         self.open()
         self.flushInput()
         self.flushOutput()
-        print("Connected to", self.port)
+        self.log.info("Connected to {}", self.port)
+        #print("Connected to", self.port)
+        
+        atexit.register(self.close)
 
     def set_gain(self, value):
         """
         Set the gain
         """
-        print("gain set to {}", value)
+        self.log.info("Gain set to {}", value)
+        #print("gain set to {}", value)
         return self.send('g {}'.format(value)), value
 
     def set_filter_corner(self, value):
         """
         Set the filter 3d corner
         """
-        print("corner set to {}", value)
+        self.log.info("Corner set to {}", value)
+        #print("corner set to {}", value)
         return self.send('f {}'.format(value)), value
 
     def set_sample_period(self, period_us):
         """
         Set the sampling period to period_us (in microseconds)
         """
-        print("period set to {}", period_us)
+        self.log.info("Period set to {}", period_us)
+        #print("period set to {}", period_us)
         return self.send('p {}'.format(period_us)), period_us
 
     def sample(self):
@@ -73,17 +74,17 @@ class LoadCellDeviceControl(serial.Serial):
         return self.send('e')
 
     def send(self, cmd):
-        if self.verbose: print('Sent: ' + cmd)
+        self.log.debug("Sent: {}", cmd)
+        #if self.verbose: print('Sent: ' + cmd)
         self.write(bytes(cmd + '\n', encoding='ascii')) # write to serial tx buffer
         response = self.receive()
-        if self.verbose: print("Response: ", response)
+        self.log.debug("Response: {}", response)
+       # if self.verbose: print("Response: ", response)
         return response                                 # return the response to the command
 
     def receive(self):
         response = b''
         response += self.readline()         # wait for the first line to fill in the rx buffer
-        #while self.in_waiting:              # while there is more data in the rx buffer
-        #    response += self.readline()     # read next line from rx buffer
         return response.decode().rstrip()   # return decoded byte response (as string) without traililng newline
         
     def receiveAll(self):
@@ -97,13 +98,14 @@ class LoadCell:
     def __init__(self):
         self.running = False
         self.thread = threading.Thread(target=self.loop)
-        #self.thread.start()
-        #self.start()
-        #self.indexs, self.micros, self.raws = [], [], []
         self.raws = []
-        self.cell = LoadCellDeviceControl(verbose=False)
+        self.cell = LoadCellDeviceControl(v)
+        
+        self.log = logging.getLogger(__name__)
+        self.log.setLevel(log_level)
 
-    def start(self):
+    def start(self, filename):
+        self.log_file = filename
         
         self.cell.connect()
         self.cell.set_sample_period(1000)
@@ -111,7 +113,6 @@ class LoadCell:
         self.cell.set_filter_corner(1000)
         
         self.running = True
-        #self.indexs, self.micros, self.raws = [], [], []
         self.raws = []
         if not self.thread.is_alive():
             self.thread.start()
@@ -140,8 +141,7 @@ class LoadCell:
     def adcToForce(self, x):
         slope = -1.46
         intercept = 33845.0
-        #slope = .48
-        #intercept = 33247
+
         grams = (x - intercept)/slope
         n = grams/1000*9.8
         return n
@@ -157,26 +157,9 @@ class LoadCell:
         #connect to it and set our gain
         
         if(self.running):
-            date_and_time = datetime.now().strftime('%Y_%m_%d__%H_%M_%S')
-
-            # get path to 3D_printer_control
-            #self.tcp_log = Path.cwd() / 'logs' / date_and_time
-            self.tcp_log = Path.cwd() / 'logs'
-
-            #print(self.tcp_log)
-            # create dir if not there
-            #try:
-            #    os.mkdir(self.tcp_log)
-            #except OSError:
-            #    print ("Creation of the directory %s failed" % self.tcp_log)
-            #else:
-            #    print ("Successfully created the directory %s " % self.tcp_log)
-
-            # create logs
-            self.tcp_log = str(self.tcp_log / 'load_cell_data_{}.txt'.format(date_and_time))
 
             #print header
-            with open(self.tcp_log, "a") as f:
+            with open(self.log_file, "a") as f:
                 f.write("Index\tMicroseconds\tRaw\tNewtons\tAvg\n")
 
             #sample until running is set to false
@@ -185,7 +168,7 @@ class LoadCell:
             while(self.running):
                 self.getData()
                 
-            with open(self.tcp_log, "a") as f:
+            with open(self.log_file, "a") as f:
                 length = len(self.raws)
                 windowSize = 10
                 windowData = []
@@ -202,6 +185,7 @@ class LoadCell:
                             data = float(data)
                             force = self.adcToForce(data)
                         except ValueError:
+                            self.log.warning("Unable to parse loadcell data: {}", self.raws[i])
                             print(self.raws[i])
                             continue
                             
