7dee58862381e5ff9046fd20a07c4346aa53ab66
Dallin Miner <keyofd.miner@gmail.com>
2021-07-08 11:23:39 -0600

    Add async file handler class

---
+++ printer_server/async_file_handler.py
@@ -0,0 +1,48 @@
+import threading, queue, time
+
+
+class AsyncFileHandler:
+    def __init__(self):
+        self.queues = {}
+        self.thread_stopped = threading.Event()
+        self.thread = None
+
+    def start(self):
+        if self.thread is None:
+            self.thread_stopped.clear()
+            self.thread = threading.Thread(target=self.loop)
+            self.thread.start()
+
+    def write(self, filename, msg):
+        if filename not in self.queues:
+            open(filename, "w").close()
+            self.queues[filename] = queue.Queue()
+        self.queues[filename].put(msg)
+
+    def finish(self):
+        if self.thread is not None:
+            for key in self.queues.keys():
+                q = self.queues[key]
+                q.join()
+                del q
+            self.queues = {}
+            self.thread_stopped.set()
+            self.thread.join()
+            self.thread = None
+
+    def loop(self):
+        while True:
+            if self.thread_stopped.is_set():
+                break
+            for key in self.queues.keys():
+                q = self.queues[key]
+                if not q.empty():
+                    with open(key, "a") as f:
+                        while not q.empty():
+                            msg = q.get()
+                            f.write(msg)
+                            q.task_done()
+            time.sleep(0.01)
+
+
+async_file_hander = AsyncFileHandler()
