36a4f431ca3661b66b1cb275fb59f6e0c06b12b8
Dallin Miner <keyofd.miner@gmail.com>
2021-09-17 15:37:23 -0600

    Fix flask_db_setup script

--- rpi/flask_db_setup.sh
+++ rpi/flask_db_setup.sh
@@ -7,11 +7,14 @@ PROJECT_ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )"/.. && pwd )"
 . $PROJECT_ROOT/env/bin/activate
 
 # Open printer server folder
-cd $PROJECT_ROOT/printer_server
+# cd $PROJECT_ROOT/printer_server
+cd $PROJECT_ROOT
 
 # Remove old database if it exists
+# [ -e dev.db ] && echo "Moving old db to dev.db.bak" && mv dev.db dev.db.bak
 [ -e migrations ] && echo "Moving old migrations to migrations.bak" && mv migrations migrations.bak
-[ -e dev.db ] && echo "Moving old db to dev.db.bak" && mv dev.db dev.db.bak
+[ -e 3d_printer_database.db ] && echo "Moving old db to 3d_printer_database.db.bak" && mv 3d_printer_database.db 3d_printer_database.db.bak
+
 
 # # Set flask app pointer
 export FLASK_APP=autoapp.py
@@ -22,4 +25,4 @@ flask db migrate
 flask db upgrade
 
 # Go back to original folder
-cd $PROJECT_ROOT
+# cd $PROJECT_ROOT
