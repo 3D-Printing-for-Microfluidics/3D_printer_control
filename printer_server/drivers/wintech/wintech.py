304376defee30ee238ab7e0e32c6413bb42beed0
Dallin Miner <keyofd.miner@gmail.com>
2021-09-27 15:43:12 -0600

    Remove virtual screen from wintech

--- printer_server/drivers/wintech/wintech.py
+++ printer_server/drivers/wintech/wintech.py
@@ -1,69 +1,57 @@
-"""Wintech optical engine control module."""
+# -*- coding: utf-8 -*-
+"""
+Visitech and Wintech optical engine control code
+=========
+"""
 import time
 import atexit
 
-from ..screen import ScreenThread
 from .usb_driver import WintechUSB
 
-
 class Wintech:
-    """Control class for the Wintech optical engine."""
-
-    def __init__(self, resolution=(1920, 1080), fullscreen=True, verbose=False):
-        self.resolution = resolution
-        self.fullscreen = fullscreen
+    """ Control module for the Wintech optical engine
+    """
+    def __init__(self, verbose=False):
         self.dmd_controller = WintechUSB(verbose=verbose)
-        self.screenThread = ScreenThread(self.resolution, self.fullscreen)
         self.ledPower = 0
-        atexit.register(self.screenThread.stop)
         atexit.register(self.dmd_controller.stopSequence)
         atexit.register(self.dmd_controller.ledOff)
 
+    # start the screen thread and dmd_controller
     def connect(self, quick=False):
-        """Start the screen thread and connect to the DMD controller."""
-        self.screenThread.start()
         self.dmd_controller.connect(quick)
 
-    def projectMulti(self, images, exposureTimes, ledPowers):
-        """Project multiple images with its own exposure time and
-        and LED power setting.
 
-        images: A list of image filenames.
-        exposureTimes: A list of exposure times (ms).
-        ledPowers: A list of led power settings (0-1000).
+    def project(self, exposure, repeat=1, ledPower=100):
+        """Project a image for a period of exposure (ms).
+        :param image: an 8-bit grayscale image filename
+        :param int exposure: exposure time (ms)
+        :param int ledPower: LED power setting (0-100)
         """
-        for image, exposure, ledPower in zip(images, exposureTimes, ledPowers):
-            self.project(image, exposure, ledPower)
 
-    def project(self, image, exposure, repeat=1, ledPower=100):
-        """Project an image for a period of exposure (ms). Breaks
-        exposure times longer than 10,000 into a series of shorter
-        exposures.
-
-        image: An 8-bit grayscale image filename.
-        exposure: Exposure time (ms).
-        repeat: How many times to repeat the exposure. 0 means repeat
-            forever.
-        ledPower: LED power setting (0-100).
-        """
+        # break exposures larger than 10,000 into a series of shorter exposures
         max_time = 10000
         n = int(exposure // max_time)
         if exposure % max_time != 0:
             exposure = [max_time] * n + [exposure % max_time]
         else:
             exposure = [max_time] * n
+
+        # update LED power if necessary
         if ledPower != self.ledPower:
             self.dmd_controller.setLedPower(ledPower)
-        for t in exposure:
-            self.dmd_controller.definePattern(t)
-            self.dmd_controller.configurePatternLut(repeat=repeat)
-            self.screenThread.screen.draw(image)
-            time.sleep(0.1)
-            self.dmd_controller.startSequence()
-            if repeat != 0:
-                time.sleep(t * 0.001 + 0.1)
-                self.dmd_controller.stopSequence()
 
-    def clear(self):
-        """Clear the projector screen to black."""
-        self.screenThread.screen.clear()
+        # run each shorter exposure
+        for t in exposure:
+            self.dmd_controller.definePattern(t)                    # must define pattern, then configure the LUT
+            self.dmd_controller.configurePatternLut(repeat=repeat)  # defaults to 1 pattern, repeat once
+            time.sleep(0.1)
+
+            self.dmd_controller.startSequence()                     # send the start command to the DLPC900 controller
+
+            if repeat != 0:                                         # if not repeating forever
+                time.sleep(t * .001 + 0.1)                          # wait for the exposure to complete before issuing stop command
+                self.dmd_controller.stopSequence()                  # issue stop command to DLPC900
+
+    def stop(self):
+        self.dmd_controller.stopSequence()
