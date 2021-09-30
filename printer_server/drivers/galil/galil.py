78de6011c8b7fba8608a66dcde3de97c40b92e5e
matthew-viglione <19675897+matthew-viglione@users.noreply.github.com>
2021-09-30 16:24:49 -0600

    Convert comments to docstrings

--- printer_server/drivers/galil/galil.py
+++ printer_server/drivers/galil/galil.py
@@ -6,17 +6,19 @@ import json
 import atexit
 import logging
 from datetime import datetime
+import gclib
 from printer_server.async_file_handler import async_file_hander
 
-# remove bad chars from file name
+
 def cleanFileName(name):
+    """Return a file name without any problematic characters."""
     for c in '\\/:*?"<>| ':
         name = name.replace(c, "")
     return name
 
 
-# maps X,Y,Z to A,B,C
 def convertAxis(axis):
+    """Return converted axis name (maps X,Y,Z to A,B,C)"""
     axis = axis.upper()
     if axis in ("X", "A"):
         return "A"
@@ -27,8 +29,11 @@ def convertAxis(axis):
     raise ValueError("Invalid axis supplied")
 
 
-# return the value for the specified axis
 def parseResponseString(string, axis="A"):
+    """Return an integer representing the value for the specified axis.
+
+    i.g. "12, 15, 20" would return "12" for axis A, "15" for B, etc.
+    """
     string = string.replace(",", "")
     array = string.split()
     axis = convertAxis(axis)
@@ -46,10 +51,6 @@ class Galil:
         bottom_position=None,
         calibration_position=None,
     ):
-
-        # import here so test system doesn't have to install gclib
-        import gclib
-
         self.log = logging.getLogger(__name__)
         self.log.setLevel(log_level)
         self.movement_log = None
@@ -67,8 +68,8 @@ class Galil:
 
         # default configuration parameters
         self.axes = ["A"]
-        self.travel = {"A": 100}  # max travel in mm
-        self.ctspmm = {"A": 8000}  # counts/mm for each axis
+        self.max_travel_mm = {"A": 100}
+        self.ctspmm = {"A": 8000}
         self.data = {}
         self.move_num = 0
 
@@ -94,13 +95,11 @@ class Galil:
         self.absMove(speed=25, cnts=self.bottom_position)
         return self.getPosition()
 
-    # find and connect to the Galil controller
     def connect(self):
-        # Get Ethernet controllers requesting IP addresses
+        """Find the first Galil controller and connect to it."""
         self.log.info("Searching for %s controller...", self.controller_name)
         available = self.g.GAddresses()
         self.address = None
-        # If there is more than one controller connected, this will only connect to the first
         for address in sorted(available.keys()):
             if self.controller_name in available[address]:
                 self.address = address.strip("()")
@@ -117,18 +116,20 @@ class Galil:
         self.log.critical(msg)
         sys.exit(msg)
 
-    # convert mm to counts for the specified axis
     def mmToCnts(self, mm, axis="A"):
-        axis = convertAxis(axis)
-        return int(mm * self.ctspmm[axis])
+        """Convert mm to counts for the specified axis."""
+        return int(mm * self.ctspmm[convertAxis(axis)])
 
-    # convert counts to mm for the specified axis
     def cntsToMm(self, counts, axis="A"):
-        axis = convertAxis(axis)
-        return counts / self.ctspmm[axis]
+        """Convert counts to mm for the specified axis."""
+        return counts / self.ctspmm[convertAxis(axis)]
 
-    # send a command to the controller, interpret errors if any are thrown
     def send(self, command, notify=True):
+        """Send a command to the controller.
+
+        If an error is returned, request and also return more
+        information about the error.
+        """
         if notify:
             self.log.debug("Sent : '%s'", command)
         try:
@@ -144,79 +145,88 @@ class Galil:
             self.log.error("Last command '%s' returned error '%s'", command, error)
             return error
 
-    # check both limit switches, return a tuple with their trip values
     def checkLimits(self, axis="A"):
+        """Return a tuple the state of the limit switches for the
+        specified axis.
+        """
         a = convertAxis(axis)
         lf = self.send("MG _LF{}".format(a), notify=False)
         lr = self.send("MG _LR{}".format(a), notify=False)
         return bool(lf == "0.0000"), bool(lr == "0.0000")
 
-    # read the current position of the specified encoder
     def getPosition(self, axis="A", notify=True):
+        """Return the position of the specified encoder."""
         if axis is None:
             return self.send("TP", notify=notify)
         axis = convertAxis(axis)
         pos = self.send("TP{}".format(axis), notify=notify)
         return int(pos)
 
-    # turn on the specified axis
     def motorOn(self, axis="A"):
+        """Turn on the specified axis."""
         axis = convertAxis(axis)
         self.send("SH{}".format(axis))
 
-    # turn off the specified axis
     def motorOff(self, axis="A"):
+        """Turn off the specified axis."""
         axis = convertAxis(axis)
         self.log.warning("Axis %s motor turned off. It may sink due to gravity.", axis)
         self.send("MO{}".format(axis))
 
-    # get the acceleration for the specified axis (mm/sec^2)
     def getAcceleration(self, axis="A"):
+        """Return the acceleration of the specified axis (mm/sec^2)."""
         a = convertAxis(axis)
         response = self.send("AC ?", notify=False)
         acc = parseResponseString(response, a)
         return int(acc) / self.ctspmm[a]
 
-    # set the acceleration for the specified axis (mm/sec^2)
     def setAcceleration(self, acceleration, axis="A"):
+        """Set the acceleration for the specified axis (mm/sec^2)."""
         a = convertAxis(axis)
         self.send("AC{}={}".format(a, acceleration * self.ctspmm[a]))
         self.send("DC{}={}".format(a, acceleration * self.ctspmm[a]))
 
-    # get the speed for the specified axis (mm/sec)
     def getSpeed(self, axis="A"):
+        """Return the speed for the specified axis (mm/sec)."""
         a = convertAxis(axis)
         response = self.send("SP ?", notify=False)
         speed = parseResponseString(response, a)
         return int(speed) / self.ctspmm[a]
 
-    # set the speed for the specified axis (mm/sec)
     def setSpeed(self, speed, axis="A"):
+        """Set the speed for the specified axis (mm/sec)."""
         a = convertAxis(axis)
         self.send("SP{}={}".format(a, speed * self.ctspmm[a]))
 
-    # run the Galil homing routine
     def home(self, axis="A"):
+        """Run the homing routine.
+
+        The homing routine begins by jogging up until the limit switch
+        is triggered, then runs the built in "HM" routine and waits for
+        motion to complete.
+        """
         self.log.info("Start homing...")
         a = convertAxis(axis)
-        # if the server was improperly shutdown, the speed might need resetting
         self.setSpeed(10)
-        self.motorOn()  # turn motor on
-        self.startJog(speed=-15)  # move up until the limit switch is triggered
-        self.g.GMotionComplete(a)  # block until motion planning is complete
-        self.stopJog()  # restores pre-jog speed
-        self.motorOn()  # turn motor back on (limit switch was tripped, which turns it off)
-        self.send("HM")  # send home command
-        self.send("BGA")  # start homing
-        # block until motion is complete (encoder is set to 0 at end of homing)
+        self.motorOn()
+        self.startJog(speed=-15)
+        self.g.GMotionComplete(a)
+        self.stopJog()
+        self.motorOn()
+        self.send("HM")
+        self.send("BGA")
         self.waitForMotionComplete(0)
         self.g.GMotionComplete(axis)
-        self.homed = True  # update class homed status
+        self.homed = True
         self.log.info("Homing complete.")
 
-    # blocking call to relative move an axis the specified distance at speed (in mm/sec)
     # pylint: disable=too-many-arguments
     def relMove(self, mm=None, cnts=None, speed=None, acceleration=None, axis="A"):
+        """Perform a relative movement.
+
+        Blocks execution until movement is complete. All units are in mm
+        and mm/sec(^2).
+        """
         a = convertAxis(axis)  # check that the axis is valid
         old_speed = None
         old_acceleration = None
@@ -240,7 +250,6 @@ class Galil:
             self.setAcceleration(old_acceleration)
         return self.getPosition()
 
-    # blocking call to move specified axis to absolute position at speed (in mm/sec)
     # pylint: disable=too-many-arguments
     def absMove(
         self,
@@ -251,6 +260,12 @@ class Galil:
         wait_for_settling=True,
         axis="A",
     ):
+        """Perform an absolute movement.
+
+        Blocks execution until movement is complete. All units are in mm
+        and mm/sec(^2). wait_for_settling determines how precise
+        the movement has to be.
+        """
         if not self.homed:
             msg = "Must home before using absolute movements!"
             self.log.critical(msg)
@@ -277,8 +292,8 @@ class Galil:
             self.setAcceleration(old_acceleration)
         return self.getPosition()
 
-    # nonbocking call that start the axis moving at a specified speed.
     def startJog(self, speed=None, axis="A"):
+        """Start a jog, non-blocking."""
         if not self.jogging:
             self.pre_jog_speed = self.getSpeed()  # save the speed before jogging begins
         self.jogging = True
@@ -287,17 +302,18 @@ class Galil:
         self.send("JG{}={}".format(a, speed * self.ctspmm[a]))
         self.send("BG{}".format(a))
 
-    # nonbocking call that stops the motion of the stage.
     def stopJog(self, axis="A"):
+        """Stop a jog, non-blocking."""
         a = convertAxis(axis)
         self.log.info("Stop jog on axis %s", a)
         self.send("ST{}".format(a))
         self.jogging = False
         self.setSpeed(self.pre_jog_speed)
 
-    # block execution until the encoder reading reaches target.
-    # Also notifys all position data
     def waitForMotionComplete(self, cnts, wait_for_settling=True, axis="A"):
+        """Blocks execution until the encoder reaches the target value
+        and saves motion data as it goes.
+        """
         start_time = time.time()
         last_position = self.getPosition(notify=False)  # save the last position
         self.data[self.move_num] = []
@@ -385,8 +401,8 @@ class Galil:
         self.data[self.move_num].append({"duration": time.time() - start_time})
         self.move_num = self.move_num + 1
 
-    # save motion data to a file
     def saveMotionData(self, filename=None):
+        """Dump the motion data to a file."""
         # save the coefficients for this run
         for coeff in ("KP", "KI", "KD", "IL"):
             response = self.send("{} ?,?,?".format(coeff))
@@ -403,8 +419,8 @@ class Galil:
         with open(filename, "w") as outfile:
             json.dump(self.data, outfile)
 
-    # end connection
     def disconnect(self):
+        """Disconnect form the Galil controller."""
         if self.connected is not False:
             try:
                 self.connected = False
@@ -413,14 +429,17 @@ class Galil:
             except self.gclib_error as e:
                 self.log.error("Unexpected GclibError on disconnect: %s", e)
 
-    # downlaod a DMC file to the controller
     def downloadProgram(self, filename):
+        """Download a DMC file to the Galil controller."""
         self.log.info("Downloading '%s' to controller...", filename)
         return self.g.GProgramDownloadFile(filename)
 
-    # interactive mode - will return a prompt you can issue Galil commands to.
-    # Exits with KeyboardInterrupt
     def interactiveMode(self):
+        """Start interactive mode.
+
+        This will leave you on a python prompt that forwards commands to
+        the controller. Exits with KeyboardInterrupt.
+        """
         if not self.connected:
             msg = "Must be connected to Galil controller to run interactive mode"
             self.log.critical(msg)
@@ -434,10 +453,10 @@ class Galil:
             print("\nExited by KeyboardInterrupt")
 
     def set_log_file(self, filename):
+        """Set the log file."""
         self.movement_log = filename
 
 
-# runs if called from the console
 if __name__ == "__main__":
     g = Galil(log_level=logging.DEBUG)
     g.connect()
