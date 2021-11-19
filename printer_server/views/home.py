1c58e6c6fcc0d8211aa938a43aae4cb0cd44fe29
Dallin Miner <keyofd.miner@gmail.com>
2021-11-19 14:06:05 -0700

    Work on #78

--- printer_server/views/home.py
+++ printer_server/views/home.py
@@ -608,6 +608,7 @@ class PrintControl:
         self.write_to_event_log(f"Tip: {tip}")
         tilt = position["tilt"]
         self.write_to_event_log(f"Tilt: {tilt}")
+        self.focused_position = float(position["distance"])
 
         # update frontend progress bar
         self.state = "printing"
