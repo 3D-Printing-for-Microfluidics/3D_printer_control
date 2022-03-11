cf1d92511c96b13e17cdc5f3ca7336e060e15171
Dallin Miner <keyofd.miner@gmail.com>
2022-03-11 10:12:25 -0700

    Rename visitech buttons and sockets

--- printer_server/drivers/visitech/visitech_snip.js
+++ printer_server/drivers/visitech/visitech_snip.js
@@ -1,34 +1,34 @@
 var enable_le_buttons = function () {
-    $('.light-engine button').prop('disabled', false);
+    $('.visitech button').prop('disabled', false);
 }
 
 var disable_le_buttons = function () {
-    // $('.light-engine button').prop('disabled', true);
+    // $('.visitech button').prop('disabled', true);
 }
 
 var disable_project_start_button = function () {
-    // $('#le-start-btn').prop('disabled', true);
+    // $('#visitech-start-btn').prop('disabled', true);
 }
 
 var enable_project_start_button = function () {
-    $('#le-start-btn').prop('disabled', false);
+    $('#visitech-start-btn').prop('disabled', false);
 }
 
 var enable_project_start_stop_buttons = function () {
-    $('#le-start-btn').prop('disabled', false);
-    $('#le-stop-btn').prop('disabled', false);
+    $('#visitech-start-btn').prop('disabled', false);
+    $('#visitech-stop-btn').prop('disabled', false);
 }
 
 var disable_project_start_stop_buttons = function () {
-    // $('#le-start-btn').prop('disabled', true);
-    // $('#le-stop-btn').prop('disabled', true);
+    // $('#visitech-start-btn').prop('disabled', true);
+    // $('#visitech-stop-btn').prop('disabled', true);
 }
 
 $(document).ready(function () {
     // Handles to user inputs
-    var LedPowerSliderElement = document.getElementById("led-power-slider");
-    var LedPowerLabelElement = document.getElementById("led-power-label");
-    var exposureElement = document.getElementById("exposure-txt");
+    var LedPowerSliderElement = document.getElementById("visitech-led-power-slider");
+    var LedPowerLabelElement = document.getElementById("visitech-led-power-label");
+    var exposureElement = document.getElementById("visitech-exposure-txt");
 
     // Set initial LED power slider label value
     LedPowerLabelElement.innerHTML = LedPowerSliderElement.value; // Display the default slider value
@@ -39,12 +39,12 @@ $(document).ready(function () {
     }
 
     // Light engine control stop button click function
-    $("#le-stop-btn").click(function () {
-        socket.emit("light_engine_stop");
+    $("#visitech-stop-btn").click(function () {
+        socket.emit("visitech_stop");
     });
 
     // Exposure text input change function
-    $('#exposure-txt').on('change', function () {
+    $('#visitech-exposure-txt').on('change', function () {
         exposure = exposureElement.value;
 
         // Validate user input. Only allows positive integers > 0
@@ -58,12 +58,12 @@ $(document).ready(function () {
     })
 
     // Light engine control start button click function
-    $("#le-start-btn").click(function () {
-        var repeatCheckboxElement = document.getElementById("repeat-chkbx");
+    $("#visitech-start-btn").click(function () {
+        var repeatCheckboxElement = document.getElementById("visitech-repeat-chkbx");
         var exposure = exposureElement.value;
         var repeat = Number(!repeatCheckboxElement.checked);
         var ledPower = LedPowerSliderElement.value;
-        socket.emit("light_engine_start", { "repeat": repeat, "exposure": exposure, "ledPower": ledPower });
+        socket.emit("visitech_start", { "repeat": repeat, "exposure": exposure, "ledPower": ledPower });
     });
 
     
--- printer_server/drivers/visitech/visitech_snip.py
+++ printer_server/drivers/visitech/visitech_snip.py
@@ -9,28 +9,28 @@ visitech = driver_handles.visitech
 imagePath = os.path.join(Config.UPLOAD_FOLDER, "calibration_images", "temp.png")
 
 
-@socketio.on("light_engine_stop", namespace="/manual")
-def lightEngineStop():
+@socketio.on("visitech_stop", namespace="/manual")
+def visitechStop():
     """Turn off the LED in the light engin."""
     visitech.stop_sequencer()
-    socketio.emit("light_engine_stop_complete", namespace="/manual", broadcast=True)
+    socketio.emit("visitech_stop_complete", namespace="/manual", broadcast=True)
 
 
-@socketio.on("light_engine_start", namespace="/manual")
-def lightEngineProject(message):
+@socketio.on("visitech_start", namespace="/manual")
+def visitechProject(message):
     """Project the image with the given settings."""
     ledPower = int(message["ledPower"])
     repeat = int(message["repeat"])
     exposure = int(message["exposure"])
     driver_handles.screen.draw(imagePath)
     visitech.project(exposure, ledPower, repeat)
-    socketio.emit("light_engine_start_complete", namespace="/manual", broadcast=True)
+    socketio.emit("visitech_start_complete", namespace="/manual", broadcast=True)
 
 
-@socketio.on("light_engine_get_status", namespace="/manual")
-def lightEngineStatus():
+@socketio.on("visitech_get_status", namespace="/manual")
+def visitechStatus():
     socketio.emit(
-        "light_engine_status",
+        "visitech_status",
         visitech.read_all_status(),
         namespace="/manual",
         broadcast=True,
