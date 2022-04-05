b63e07dea6266376455173d1c209bd90dcabe826
Dallin Miner <keyofd.miner@gmail.com>
2022-04-05 10:58:33 -0600

    Increase visibility of screen buttons

--- printer_server/drivers/screen/screen_snip.js
+++ printer_server/drivers/screen/screen_snip.js
@@ -1,9 +1,29 @@
 var enable_upload_button = function (le) {
     $(`#${le}-upload-btn`).prop('disabled', false);
+    $(`#${le}-upload-btn`).addClass('btn-outline-info');
+    $(`#${le}-upload-btn`).removeClass('btn-outline-secondary');
 }
 
 var disable_upload_button = function (le) {
     $(`#${le}-upload-btn`).prop('disabled', true);
+    $(`#${le}-upload-btn`).removeClass('btn-outline-info');
+    $(`#${le}-upload-btn`).addClass('btn-outline-secondary');
+}
+
+var highlight_draw_button = function (le) {
+    $(`#${le}-draw-btn`).addClass('active');
+}
+
+var unhighlight_draw_button = function (le) {
+    $(`#${le}-draw-btn`).removeClass('active');
+}
+
+var highlight_draw_white_button = function (le) {
+    $(`#${le}-white-btn`).addClass('active');
+}
+
+var unhighlight_draw_white_button = function (le) {
+    $(`#${le}-white-btn`).removeClass('active');
 }
 
 $(document).ready(function () {
@@ -24,7 +44,7 @@ $(document).ready(function () {
 
     for (var le of hardware["screen"]["light_engines"]) {
 
-        $(`#${le}-upload-btn`).prop('disabled', true);
+        disable_upload_button(le);
 
         document.getElementById(`${le}-file-picker`).addEventListener('change', function (event) {
             var projector = $(this).closest(".row").attr('aria-label');
@@ -45,6 +65,7 @@ $(document).ready(function () {
             if (typeof selectedFile !== 'undefined') { // if there is a file selected
                 uploadFile(selectedFile, projector);
                 disable_upload_button(projector);
+                unhighlight_draw_button(projector);
             }
         });
 
@@ -52,18 +73,24 @@ $(document).ready(function () {
         $(`#${le}-white-btn`).on("click", function (e) {
             var projector = $(this).closest(".row").attr('aria-label');
             socket.emit("screen_white", { "light_engine": projector });
+            highlight_draw_white_button(projector);
+            unhighlight_draw_button(projector);
         });
 
         // Draw button click function
         $(`#${le}-draw-btn`).on("click", function (e) {
             var projector = $(this).closest(".row").attr('aria-label');
             socket.emit("screen_draw", { "light_engine": projector });
+            highlight_draw_button(projector);
+            unhighlight_draw_white_button(projector);
         });
 
         // Clear button click function
         $(`#${le}-clear-btn`).on("click", function (e) {
             var projector = $(this).closest(".row").attr('aria-label');
             socket.emit("screen_clear", { "light_engine": projector });
+            unhighlight_draw_button(projector);
+            unhighlight_draw_white_button(projector);
         });
     };
 });
