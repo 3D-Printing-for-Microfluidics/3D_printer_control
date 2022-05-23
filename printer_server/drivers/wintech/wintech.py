bb2ab82bc465da9817709e35ef512d8c0f0c2356
Dallin Miner <keyofd.miner@gmail.com>
2022-05-23 09:56:01 -0600

    Fix #146

--- printer_server/drivers/wintech/wintech.py
+++ printer_server/drivers/wintech/wintech.py
@@ -13,6 +13,7 @@ class Wintech:
         self.log = logging.getLogger(__name__)
         self.log.setLevel(log_level)
         self.dmd_controller = None
+        self.repeats = 1
         self.exposure_time = 0
         self.led_on = False
 
@@ -26,36 +27,39 @@ class Wintech:
         Turn the sequencer off.
         """
         self.dmd_controller.stop_sequence()
-        self.dmd_controller.led_off()
 
-    def setup_exposure(self, t, p, r=1):
+    def setup_exposure(self, exposure_time_ms, led_power=100, repeat=1):
         """
         Setup an exposure.
             t - exposure time in milliseconds
             p - power setting
             r - number of repeats
         """
-        self.exposure_time = t
+        self.repeats = repeat
+        self.exposure_time = exposure_time_ms
         min_t = 4.046
         max_t = 10000
         self.log.info(
-            "Setting up exposure for %s ms at power setting %s. Repeat %s", t, p, r
+            "Setting up exposure for %s ms at power setting %s. Repeat %s",
+            exposure_time_ms,
+            led_power,
+            repeat,
         )
-        if t > max_t:
-            msg = f"Exposure time {t} ms is greater than maximum possible exposure time "
+        if exposure_time_ms == 0:
+            return
+        elif exposure_time_ms > max_t:
+            msg = f"Exposure time {exposure_time_ms} ms is greater than maximum possible exposure time "
             msg += f"of {max_t} ms. Using exposure time of {max_t} ms instead."
             self.log.warning(msg)
-            t = max_t
             self.exposure_time = max_t
-        elif t < min_t:
-            msg = f"Exposure time {t} ms is less than minimum possible exposure time "
+        elif exposure_time_ms < min_t:
+            msg = f"Exposure time {exposure_time_ms} ms is less than minimum possible exposure time "
             msg += f"of {min_t} ms. Using exposure time of {min_t} ms instead."
             self.log.warning(msg)
-            t = min_t
             self.exposure_time = min_t
-        self.dmd_controller.set_led_power(p)
-        self.dmd_controller.define_pattern(exposure=t)
-        self.dmd_controller.configure_pattern_LUT(repeats=r)
+        self.dmd_controller.set_led_power(led_power)
+        self.dmd_controller.define_pattern(self.exposure_time)
+        self.dmd_controller.configure_pattern_LUT(repeat=self.repeats)
 
     def perform_exposure(self):
         """
@@ -63,10 +67,12 @@ class Wintech:
         """
         self.led_on = True
         if self.exposure_time != 0:
-            self.log.info("Exposing for %s ms", self.exposure_time)
-            self.dmd_controller.start_sequence()
-            time.sleep(self.exposure_time * 1e-3)
-        self.led_on = False
+            if self.repeats == 0:
+                self.dmd_controller.start_sequence()
+            else:
+                self.dmd_controller.start_sequence()
+                time.sleep(self.exposure_time * 0.001 + 0.1)
+                self.led_on = False
 
     def project(self, exposure_time_ms, led_power=100, repeat=1):
         """Call all of the necessary methods to project an image and
@@ -87,15 +93,15 @@ class Wintech:
             repeat,
         )
         self.dmd_controller.set_led_power(led_power)
-        self.led_on = True
         if exposure_time_ms > max_t:
             self.log.warning("Exposure time is too high. Using maximum of 10 seconds.")
             exposure_time_ms = max_t
         elif min_t > exposure_time_ms:
             self.log.warning("Exposure time is too low. Using minimum of 4 milliseconds.")
             exposure_time_ms = min_t
+        self.led_on = True
         if repeat == 0:
-            self.dmd_controller.define_pattern(exposure_time_ms)
+            self.dmd_controller.define_pattern(33.1)
             self.dmd_controller.configure_pattern_LUT(repeat=repeat)
             self.dmd_controller.start_sequence()
         else:
