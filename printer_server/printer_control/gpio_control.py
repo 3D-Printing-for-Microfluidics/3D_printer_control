1ee3e4c74f7b2a679e14eb05cef895d3534ebc09
Dallin Miner <keyofd.miner@gmail.com>
2024-01-13 13:39:33 -0700

    Remove MR1 fan control (obsolete)

--- printer_server/printer_control/gpio_control.py
+++ printer_server/printer_control/gpio_control.py
@@ -18,24 +18,3 @@ class FilmGPIOControl(GPIOControl):
         self.gpio.film_relay_on()
         super().move_build_platform_up(position_settings)
         self.gpio.film_relay_off()
-
-
-class VisitechFanGPIOControl(GPIOControl):
-    def pre_print_tasks(self):
-        self.gpio.fan_relay_on()
-        super().pre_print_tasks()
-        self.gpio.fan_relay_off()
-
-    def post_print_tasks(self):
-        # always turn off the Visitech
-        self.gpio.fan_relay_off()
-        super().post_print_tasks()
-
-    def pre_exposure_tasks(self, settings, light_engine):
-        if light_engine == "wintech":
-            if self.gpio.fan_relay_state == False:
-                self.gpio.fan_relay_on()
-        else:
-            if self.gpio.fan_relay_state == True:
-                self.gpio.fan_relay_off()
-        super().pre_exposure_tasks(settings, light_engine)
