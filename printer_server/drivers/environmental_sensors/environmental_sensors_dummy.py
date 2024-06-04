513436ab26e0ec54e0206b4c2d8fcd1035117b54
Alejandro Pelaez <apd915@byu.edu>
2024-06-04 13:04:17 -0600

    Forgot to save before commiting. Has the newest additions.

--- printer_server/drivers/environmental_sensors/environmental_sensors_dummy.py
+++ printer_server/drivers/environmental_sensors/environmental_sensors_dummy.py
@@ -5,3 +5,51 @@ class Environmental_sensors:
     @dummy_log
     def __init__(self, *args, **kwargs):
         pass
+
+    @dummy_log
+    def findUsbPort(self, *args, **kwargs):
+        pass
+
+    @dummy_log
+    def connect(self, *args, **kwargs):
+        pass
+
+    @dummy_log
+    def disconnect(self, *args, **kwargs):
+        pass
+
+
+    ########################
+    # ESP32 serial wrappers
+    ########################
+            
+
+    def get_all_measurements(self, *args, **kwargs):
+        pass
+
+    
+    def get_temperature(self, *args, **kwargs):
+        pass
+    
+    def get_humidity(self, *args, **kwargs):
+        pass
+
+    def get_pressure(self, *args, **kwargs):
+        pass
+
+    def get_gas(self, *args, **kwargs):
+        pass
+
+    def get_airQuality(self, *args, **kwargs):
+        pass
+
+    def get_voc(self, *args, **kwargs):
+        pass
+
+
+
+    def send(self, *args, **kwargs):
+        pass
+    
+    def receive(self, *args, **kwargs):
+        pass
