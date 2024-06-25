8fa60c81e90a6146eaac0e9e8bcbef5dca37ee64
hannah-ee <eddy@CB494-EE11413.et.byu.edu>
2024-06-25 15:50:08 -0600

    .js updates

--- printer_server/drivers/photodiode/photodiode_snip.js
+++ printer_server/drivers/photodiode/photodiode_snip.js
@@ -11,22 +11,12 @@ var disable_button = function () {
     $(`#download-spectra-btn`).addClass('btn-outline-secondary');
 }
 
-var update_tiptilt_positions = function (message) {
-    for (var axis of axes) {
-        if (!$.isEmptyObject(message)) {
-            document.getElementById(`${axis}-state`).innerHTML = message[axis];
-        }
-    }
-}
-
-
 $(document).ready(function () {
 
 	socket.on("send_photodiode_power", function (message) {
         var pow = message[power];
         var length = message[wavelength]
         enable_button();
-
     });
 
     $("#photodiode_power").click(function () {
@@ -34,7 +24,12 @@ $(document).ready(function () {
     }); 
     
     $("#wavelength_350_405").click(function () {
-        
+        var wavelength = document.getElementById("wavelength_350_405");              
+        if (wavelength == 365) {
+            socket.emit("get_photodiode_power",{"wavelength":wavelength})
+        } else {
+            socket.emit("get_photodiode_power",{"wavelength":wavelength})
+        }
     });         
     
     $(`#read_photodiode_power`).on("click", function () {
@@ -42,14 +37,8 @@ $(document).ready(function () {
         socket.emit("get_photodiode_power",{"wavelength":length}) // ... figure out what element its sending 
     }); 
 
-
-    // ??? Put here one for reading/updating the value outputs?
-
     // $("#wavelength_350_405 :input").change(function () {
     //     socket.emit("wavelength_350_405", $(this).parent().text());
-
     // wavelenghtbtn.parent.text.... something???? Later do this? 
-    
-    // ####### Get socket on to update html element with power reading
 });
 
