c7f43235ea7e91c1206c25d24e7de9d9a9b5458d
CallumGalloway <gallocal@byu.edu>
2024-07-22 09:55:00 -0600

    structure for dummy driver

--- printer_server/drivers/accelerometer/accelerometer_dummy.py
+++ printer_server/drivers/accelerometer/accelerometer_dummy.py
@@ -5,3 +5,31 @@ class Accelerometer_dummy:
     @dummy_log
     def __init__(self, *args, **kwargs):
         pass
+
+    @dummy_log
+    def connect(self, *args, **kwargs):
+        return True
+
+    @dummy_log
+    def disconnect(self, *args, **kwargs):
+        pass
+
+    @dummy_log
+    def findUsbPort(self, *args, **kwargs):
+        pass
+
+    @dummy_log
+    def send(self, *args, **kwargs):
+        pass
+
+    @dummy_log
+    def receive(self, *args, **kwargs):
+        pass
+
+    @dummy_log
+    def receiveAll(self, *args, **kwargs):
+        pass
+
+    @dummy_log
+    def loop(self, *args, **kwargs):
+        pass
