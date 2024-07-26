da3808cfb4e292e24a29e3c455aa3ff655f7f724
Dallin Miner <keyofd.miner@gmail.com>
2024-07-26 16:46:30 -0600

    Work on ethernet driver

--- printer_server/drivers/generic_drivers/ethernet_serial.py
+++ printer_server/drivers/generic_drivers/ethernet_serial.py
@@ -1,127 +1,98 @@
+import sys
 import time
 import atexit
+import socket
 import logging
+import threading
 
 class EthernetSerial():
-    pass
-    # def __init__(
-    #     self,
-    #     config_dict=None,
-    #     log_level=logging.DEBUG,
-    # ):
-    #     self.log = logging.getLogger(__name__)
-    #     self.log.setLevel(log_level)
+    def __init__(self, host=None, port=None, line_ending='\r', logger=logging.getLogger(__name__)):
+        self.log = logger
+        self.host = host
+        self.port = port
+        self.line_ending = line_ending
+        self.socket = None
+        self.connected = None
+        self.initialized = None
+        self.sendLock = threading.Lock()
 
-    #     self.config_dict = config_dict
-    #     self.sendLock = threading.Lock()
-    #     self.connected = None
-    #     self.initialized = None
-    #     self.socket = None
+    def connect(self, shutdown):
+        """Find the device and connect to it."""
+        if self.connected is None:
+            self.connected = False
+            self.log.info("Connecting to device (%s), this may take up to 1 minute...", self.host)
 
-    # def connect(self, shutdown):
-    #     """Find the first ACS controller and connect to it."""
-    #     if self.connected is None:
-    #         attempts=10
-    #         timeout=1
+            attempts=10
+            timeout=1
+            i = 0
+            while i < attempts:  # try up to attempts number of times to create a connection
+                i += 1
+                try:
+                    self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                    self.socket.settimeout(10)
+                    self.socket.connect((self.host, self.port))
+                    self.connected = True
+                    self.shutdown = shutdown
+                except (OSError, socket.timeout) as e:
+                    if "timed out" in e:
+                        i = attempts
+                    self.log.info("%s. Retrying in %s second(s)", e, timeout)
+                    self.socket = None  # get rid of handle to bad socket
+                    time.sleep(timeout)  # wait to try again
+            if not self.connected:
+                msg = f"Device not found!"
+                self.log.critical(msg)
+                return False
             
-    #         self.connected = False
-    #         self.address = self.config_dict["address"]
-    #         self.port = self.config_dict["port"]
-    #         self.log.info("Connecting to ACS (%s), this may take up to 1 minute...", self.address)
-    #         i = 0
-    #         while i < attempts:  # try up to attempts number of times to create a connection
-    #             i += 1
-    #             try:
-    #                 self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    #                 self.socket.settimeout(10)
-    #                 self.socket.connect((self.address, self.port))
-    #                 self.connected = True
-    #                 self.shutdown = shutdown
-    #             except (OSError, socket.timeout) as e:
-    #                 if "timed out" in e:
-    #                     i = attempts
-    #                 self.log.info("%s. Retrying in %s second(s)", e, timeout)
-    #                 self.socket = None  # get rid of handle to bad socket
-    #                 time.sleep(timeout)  # wait to try again
-    #         if not self.connected:
-    #             msg = f"ACS controller not found!"
-    #             self.log.critical(msg)
-    #             return False
-            
-    #         self.thread_running = True
-    #         self.thread.start()
-    #         atexit.register(self.disconnect)
-    #         self.log.info("Connected to ACS controller")
-    #         return True
-    #     else:
-    #         while self.connected is False:
-    #             time.sleep(0.1)
+            atexit.register(self.disconnect)
+            self.log.info("Connected to device")
+            return True
+        else:
+            while self.connected is False:
+                time.sleep(0.1)
+        return None
 
-    # def disconnect(self):
-    #     """Disconnect form the ACS controller."""
-    #     if self.connected is not None and self.connected is not False:
-    #         for axis in self.axes:
-    #             self.motorOff(axis)
+    def disconnect(self):
+        """Disconnect form the device."""
+        if self.connected is not None and self.connected and self.socket is not None:
+            self.connected = None
+            self.initialized = None
+            try:
+                with self.sendLock:
+                    self.socket.close()
+                self.socket = None
+                self.log.info("Disconnected from device")
+            except:
+                self.log.error("Unexpected error on disconnect")
 
-    #         self.thread_running = False
-    #         try:
-    #             self.thread.join()
-    #         except RuntimeError:
-    #             pass
-    #         self.thread = Thread(self.log, name="acs_loop_thread", target=self.loop)
-    #         self.thread.daemon = True
+    def send(self, command, notify=True):
+        """Send a command to the device.
+        """
+        with self.sendLock:
+            if notify:
+                self.log.debug("Sent : '%s'", command)
+            command += self.line_ending
+            encoded_message = command.encode()
+            try:
+                self.socket.sendall(encoded_message)
+            except socket.timeout:
+                msg = "Message send timed out!"
+                self.log.critical(msg)
+                self.shutdown(is_critical = True)
+                sys.exit(msg)
+            except Exception as ex:
+                self.log.error("Failed to send packet: %s", ex)
 
-    #         self.connected = None
-    #         self.initialized = None
-    #         try:
-    #             if self.socket is not None:
-    #                 with self.sendLock:
-    #                     self.socket.close()
-    #                 self.socket = None
-    #             self.log.info("Disconnected from ACS controller")
-    #         except:
-    #             self.log.error("Unexpected error on disconnect")
-
-    # def send(self, command, notify=True):
-    #     """Send a command to the controller.
-
-    #     If an error is returned, request and also return more
-    #     information about the error.
-    #     """
-    #     with self.sendLock:
-    #         if notify:
-    #             self.log.debug("Sent : '%s'", command)
-    #         response = self._send(command)
-    #         response = "".join(response)
-    #         self.log.debug("Reply: '%s'", response)
-    #         if response != "":
-    #             if notify:
-    #                 self.log.debug("Reply: '%s'", response)
-    #             if response[0] == '?':
-    #                 self.log.error("Last command '%s' returned error '%s (%s)'", command, response, self.send(f"?{response}"))
-    #         return response
-                
-    # def _send(self, msg):
-    #     msg += "\r"
-    #     encoded_message = msg.encode()
-    #     try:
-    #         self.socket.sendall(encoded_message)
-    #     except socket.timeout:
-    #         msg = "ACS controller send timed out!"
-    #         self.log.critical(msg)
-    #         self.shutdown(is_critical = True)
-    #         sys.exit(msg)
-    #     except Exception as ex:
-    #         self.log.error("Failed to send packet: %s", ex)
-
-    #     try:
-    #         msg = self.socket.recv(1024).decode()
-    #         return msg.strip()[:-1].strip()
-    #     except socket.timeout:
-    #         msg = "ACS controller recieve timed out!"
-    #         self.log.critical(msg)
-    #         self.shutdown(is_critical = True)
-    #         sys.exit(msg)
-    #     except Exception as e:
-    #         self.log.error("Failed to receive packet: %s", e)
-    #     return None
+            try:
+                rsp = self.socket.recv(1024).decode()
+                if notify:
+                    self.log.debug("Reply: '%s'", rsp)
+                return rsp
+            except socket.timeout:
+                msg = "Message recieve timed out!"
+                self.log.critical(msg)
+                self.shutdown(is_critical = True)
+                sys.exit(msg)
+            except Exception as e:
+                self.log.error("Failed to receive packet: %s", e)
+            return None
--- printer_server/drivers/keyence/keyence.py
+++ printer_server/drivers/keyence/keyence.py
@@ -3,12 +3,9 @@ Keyence Confocal Displacement Sensor
 ====================================
 """
 import logging
-import json
-import atexit
-import socket
-import time
+from printer_server.drivers.generic_drivers import EthernetSerial
 
-class Keyence:
+class Keyence(EthernetSerial):
     def __init__(
         self,
         config_dict=None,
@@ -17,65 +14,10 @@ class Keyence:
         self.log = logging.getLogger(__name__)
         self.log.setLevel(log_level)
 
-
-        self.connected = False
-        self.socket = None
-        self.host = config_dict["ip_addr"]
-        self.port = config_dict["port"]
-        
-    def connect(self):
-        attempts=10
-        timeout=1
-        self.log.info("Connecting to Keyence sensors, this may take up to 1 minute...")
-
-        # start TCP connection
-        i = 0
-        self.connected = False
-        while i < attempts:  # try up to attempts number of times to create a connection
-            i += 1
-            try:  # attempt a new connection
-                self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-                self.socket.settimeout(10)
-                self.socket.connect((self.host, self.port))
-                self.connected = True
-            except (OSError, socket.timeout) as e:
-                if "timed out" in str(e):
-                    i = attempts
-                self.log.info("%s. Retrying in %s second(s)", e, timeout)
-                self.socket = None  # get rid of handle to bad socket
-                time.sleep(timeout)  # wait to try again
-        if not self.connected:  # connection failed every time, notify user
-            msg = "Keyence sensor not found! (CL-3000)"
-            self.log.critical(msg)
-            return False
-
-        # register exit handlers
-        atexit.register(self.disconnect)
-        self.log.info("Connected to Keyence sensor")
-        return True
-
-    def disconnect(self):
-        if self.connected and self.socket is not None:
-            self.socket.close()
-            self.connected = False
-            self.socket = None
-            self.log.info("Disconnected from Keyence sensor")
-
-    def send_command(self, message):
-        message += "\r"
-        encoded_message = message.encode()
-        try:
-            self.socket.sendall(encoded_message)
-        except Exception as ex:
-            self.log.error(f"Failed to send message to sensor due to {ex}")
-        else:
-            self.log.debug(f"Command sent: {message}")
-        response = self.socket.recv(1024).decode()
-        self.log.debug(f"Feedback: {response}")
-        return response
+        super().__init__(host=config_dict["address"], port=config_dict["port"], logger=self.log)
 
     def read_all(self):
-        data = self.send_command("MA,0").split(",")[1:]
+        data = self.send("MA,0").split(",")[1:]
         return data
 
     def read_sensor(self, index):
