36cab022274265d173df0800dd5d5ae6cd1e24fe
Dallin Miner <keyofd.miner@gmail.com>
2024-12-30 18:17:15 -0700

    Actually call TTR logging start/stop

--- printer_server/printer_control/ttr_control.py
+++ printer_server/printer_control/ttr_control.py
@@ -2,7 +2,7 @@ import logging
 
 from printer_server.threading_wrapper import Thread
 from printer_server.hardware_configuration.hardware_configuration import driver_handles
-from printer_server.printer_control.print_control import PrintControl, run_in_thread
+from printer_server.printer_control.print_control import PrintControl, PrintingException, run_in_thread
 from printer_server.views.calibration import (
     get_last_calibration_positions_from_logs,
 )
@@ -16,6 +16,10 @@ class TTRControl(PrintControl):
 
         # hardware handles
         self.ttr_stage = driver_handles.ttr_stage
+
+    def create_logs(self):
+        super().create_logs()
+        self.ttr_stage.setup_log_file(str(self.current_job / "logs"))
 
     def connect_hardware(self):
         self.ttr_thread = Thread(log, name="ttr_control_connect_thread", target=self.ttr_stage.connect)
@@ -67,4 +71,24 @@ class TTRControl(PrintControl):
                     if thread.exception is not None:
                         log.warning("Unable to move TTR stage")
         super().pre_print_joins()
+
+    @run_in_thread("planarizing", "Planarization Step 1")
+    def planarization_step_1(self):
+        if self.state in ["initialized", "planarized", "completed", "stopped"]:
+            try:
+                self.ttr_stage.logging_start()
+            except Exception as ex:
+                log.critical("Unable to communicate with ttr stage (%s)", ex, exc_info=True)
+                self.failed_hardware["TTR Stage"] = self.ttr_stage
+                raise PrintingException()
+            super().planarization_step_1()
         
+    def finish_print(self):
+        try:
+            self.ttr_stage.logging_stop()
+            self.ttr_stage.setup_log_file(None)
+        except Exception as ex:
+            log.critical("Unable to communicate with ttr stage (%s)", ex, exc_info=True)
+            self.failed_hardware["TTR Stage"] = self.ttr_stage
+            raise PrintingException()
+        super().finish_print()
