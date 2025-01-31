a20ec178d78a716c47b2078be7c9ff5f4bda4398
Dallin Miner <keyofd.miner@gmail.com>
2025-01-31 10:39:42 -0700

    Update light_measurement_control

--- printer_server/printer_control/light_measurement_control.py
+++ printer_server/printer_control/light_measurement_control.py
@@ -49,13 +49,14 @@ class LightMeasurementControl(PrintControl):
 
     def pre_print_tasks(self):
         super().pre_print_tasks()
-        try:
-            self.measure_light("preprint")
-        except Exception as ex:
-            log.warning("Error occured during light measurement (%s)", ex, exc_info=True)
+        if not self.printing_paused.is_set():
+            try:
+                self.measure_light("preprint")
+            except Exception as ex:
+                log.warning("Error occured during light measurement (%s)", ex, exc_info=True)
 
     def post_print_tasks(self):
-        if not self.printing_stopped.is_set():
+        if not (self.printing_stopped.is_set() or self.printing_paused.is_set()):
             try:
                 self.measure_light("postprint")
             except Exception as ex:
@@ -72,76 +73,110 @@ class LightMeasurementControl(PrintControl):
             self.xy_threads = self.xy_stage.threadedXYMove(log, x_pos, y_pos, join=False)
             self.focus_thread = self.focus_stage.threadedFocusMove(log, focus_pos, join=False)
 
+            # Join threads
+            for thread in self.xy_threads:
+                if thread is not None:
+                    thread.join()
+                    if thread.exception is not None:
+                        raise thread.exception
+            if self.focus_thread is not None:
+                self.focus_thread.join()
+                if self.focus_thread.exception is not None:
+                    raise self.focus_thread.exception
+
             # Setup light engines
             for i, wavelength in enumerate(config_dict[light_engine]["leds_nm"]):
                 light_engine_driver = self.light_engines[light_engine]
+                num_avg = config_dict["spectrometer"]["default_number_of_averages"]
+
+                try:
+                    self.screen.setCorrectionEnable(False, False, light_engine=light_engine)
+                except:
+                    continue
+
                 light_engine_thread = Thread(
                     log, 
                     name=f"{light_engine}_control_setup_thread",
                     target=light_engine_driver.setup_exposure,
-                    args=[10000, 100],
-                    kwargs={"repeat": 0, "led_num": i},
+                    args=[10000],
+                    kwargs={"led_power": 100, "repeat": 0, "is_grayscale_corrected":False, "led_num": i},
                 )
-                light_engine_thread.start()
 
-                # Draw to screen
                 imagePath = os.path.join(
                     Config.PRINT_SERVER_FOLDER, f"drivers/{light_engine}/images", f"white.png"
                 )
-                screen_index = 0
-                for i, le in enumerate(self.screen.light_engines):
-                    if le in light_engine:
-                        screen_index = i
-                        break
-
+                print(imagePath, light_engine)
                 screen_thread = Thread(
-                    log, name="screen_control_draw_thread", target=self.screen.draw, args=[imagePath], kwargs={"screen": screen_index}
+                    log, name="screen_control_draw_thread", target=self.screen.draw, args=[imagePath], kwargs={"light_engine": light_engine, "led_num": i}
                 )
+                light_engine_thread.start()
                 screen_thread.start()
 
-                # Join threads
-                for thread in self.xy_threads:
-                    if thread is not None:
-                        thread.join()
-                        if thread.exception is not None:
-                            raise thread.exception
-                if self.focus_thread is not None:
-                    self.focus_thread.join()
-                    if self.focus_thread.exception is not None:
-                        raise self.focus_thread.exception
                 light_engine_thread.join()
                 if light_engine_thread.exception is not None:
                     raise light_engine_thread.exception
                 self.screen_thread.join()
                 if self.screen_thread.exception is not None:
                     raise self.screen_thread.exception
-                update_screen_preview(
-                    light_engine, 
-                    self.screen.fetch_preview(self.screen.getScreenNumber(light_engine))
-                )
 
                 # Turn on light engine
                 update_le_led_state(light_engine, True)
                 light_engine_driver.perform_exposure()
                 time.sleep(0.1)
 
-                # Measure spectrum and irradiance
-                self.num_avg = config_dict["spectrometer"]["default_number_of_averages"]
-                self.integration_time = None
-                self.spectrum = None
-                self.irradiance = None 
-                spectrum_thread = Thread(log, name="spectrum_measure_thread", target=self.measure_spectra, args = [])
-                spectrum_thread.start()
-                # #### is this correctly calling wavelength? 
-                irradiance_thread = Thread(log, name="irradiance_measure_thread", target=self.measure_irradiance, args = [wavelength])
-                irradiance_thread.start()
+                update_screen_preview(
+                    light_engine, 
+                    self.screen.fetch_preview(light_engine)
+                )
 
-                spectrum_thread.join()
-                if spectrum_thread.exception is not None:
+                # Measure spectrum and irradiance
+                try:
+                    integration_time, spectrum = self.measure_spectra(num_avg)
+                except:
                     log.warning("Unable to measure spectra")
-                irradiance_thread.join()
-                if irradiance_thread.exception is not None:
+                try:
+                    irr1 = self.measure_irradiance(wavelength)
+                except:
                     log.warning("Unable to measure irradiance")
+
+                # Turn off light engine
+                light_engine_driver.stop_sequencer()
+                update_le_led_state(light_engine, False)
+
+                try:
+                    self.screen.setCorrectionEnable(True, False, light_engine=light_engine)
+                except Exception as ex:
+                    continue
+
+                light_engine_thread = Thread(
+                    log, 
+                    name=f"{light_engine}_control_setup_thread",
+                    target=light_engine_driver.setup_exposure,
+                    args=[10000],
+                    kwargs={"led_power": 100, "repeat": 0, "is_grayscale_corrected":True, "led_num": i},
+                )
+
+                light_engine_thread.start()
+  
+                light_engine_thread.join()
+                if light_engine_thread.exception is not None:
+                    raise light_engine_thread.exception
+
+                # Turn on light engine
+                update_le_led_state(light_engine, True)
+                light_engine_driver.perform_exposure()
+                time.sleep(0.1)
+
+                update_screen_preview(
+                    light_engine, 
+                    self.screen.fetch_preview(light_engine)
+                )
+
+                try:
+                    irr2 = self.measure_irradiance(wavelength)
+                except:
+                    log.warning("Unable to measure irradiance")  
+
                 
                 # Turn off light engine
                 light_engine_driver.stop_sequencer()
@@ -150,23 +185,29 @@ class LightMeasurementControl(PrintControl):
                 # Save spectrum to file
                 spectra_path = str(self.current_job / "logs" / f"{path_prefix}_spectra_{light_engine}_{wavelength}_nm.csv")
                 async_file_hander.write(spectra_path, f"Wavelength: {wavelength} (nm)\n")
-                async_file_hander.write(spectra_path, f"Irradiance: {self.irradiance} mW/cm^2\n")               
-                async_file_hander.write(spectra_path, f"Integration time: {self.integration_time} ms\n")
-                async_file_hander.write(spectra_path, f"Number of Averages: {self.num_avg}\n")
+                async_file_hander.write(spectra_path, f"Irradiance: {irr1} mW/cm^2\n")     
+                async_file_hander.write(spectra_path, f"Irradiance (grayscale corrected): {irr2} mW/cm^2\n")               
+                async_file_hander.write(spectra_path, f"Integration time: {integration_time} ms\n")
+                async_file_hander.write(spectra_path, f"Number of Averages: {num_avg}\n")
                 async_file_hander.write(spectra_path, "\n")
-                if self.spectrum is not None:
+                if spectrum is not None:
                     async_file_hander.write(spectra_path, "wavelength (nm),counts\n")
-                    for _wavelength, _counts in zip(self.spectrum[0], self.spectrum[1]):
+                    for _wavelength, _counts in zip(spectrum[0], spectrum[1]):
                         async_file_hander.write(spectra_path, f"{_wavelength},{_counts}\n") 
                     
-    def measure_spectra(self):
+    def measure_spectra(self, avg=1):
             log.info("Calculating spectrometer integration time")
-            self.integration_time = self.spectrometer.set_integration_time(None)
+            integration_time = self.spectrometer.set_integration_time(None)
             log.info("Measuring spectra")
-            self.spectrum = self.spectrometer.get_spectrum(self.num_avg)
+            return integration_time, self.spectrometer.get_spectrum(avg)
             
-    def measure_irradiance(self, length):
+    def measure_irradiance(self, l):
             log.info("Setting wavelength for irradiance")
-            self.photodiode.set_wavelength(length)
+            self.photodiode.set_wavelength(l)
             log.info("Measuring irradiance")   
-            self.irradiance = self.photodiode.get_power_density(log=True)
+            irradiances = []
+            for _ in range(100):
+                irradiances.append(self.photodiode.get_power_density())
+                time.sleep(0.01)
+            log.info("Irradiance is %.2f mW/cm^2", sum(irradiances)/len(irradiances))
+            return sum(irradiances)/len(irradiances)
