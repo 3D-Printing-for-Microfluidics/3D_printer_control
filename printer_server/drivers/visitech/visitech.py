13db5f4a8944a827d632681564d70daf2bc73b7a
Dallin Miner <keyofd.miner@gmail.com>
2025-08-12 12:55:50 -0600

    Add in Visitech initialization check

--- printer_server/drivers/visitech/visitech.py
+++ printer_server/drivers/visitech/visitech.py
@@ -95,17 +95,15 @@ class Visitech(EthernetSerial, LightEngineDriver):
         # set default state for light engine and clear previous errors
         self.log.info("Initializing Visitech...")
         self.get_sticky_errors(warn="NONE")
-        if not (
-            self.get_video_source() == "HDMI" and 
-            self.get_led_driver_regulation_mode() == "LIGHT" and 
-            self.get_dmd_operation_mode() == "VIDEO_PATTERN_MODE"
-        ):
+        if not self.get_video_source() == "HDMI":
             self.set_video_source("HDMI")
+        if not self.get_led_driver_regulation_mode() == "LIGHT":
             if self.dual_led:
                 self.set_led_driver_regulation_mode("LIGHT", led_num=0)
                 self.set_led_driver_regulation_mode("LIGHT", led_num=1)
             else:
                 self.set_led_driver_regulation_mode("LIGHT")
+        if not self.get_dmd_operation_mode() == "VIDEO_PATTERN_MODE":
             self.set_dmd_operation_mode("VIDEO_PATTERN_MODE")
         self.log.info("Initialized Visitech")
 
